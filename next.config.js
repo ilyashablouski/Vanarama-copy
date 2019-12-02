24479ce01e848eacab8d93a9243d38dd756bbef6
iamgnlc <gianluca.agnocchetti@autorama.co.uk>
2019-12-02 14:23:32 +0000

    wip

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
+  devIndicators: {
+    autoPrerender: false,
+  },
+
+  webpack: (config) => {
+    // Allow absolute imports.
+    config.resolve.modules = [...config.resolve.modules, "."]
+
+    // Fixes npm packages that depend on `fs` module.
     config.node = {
-      fs: 'empty'
+      fs: "empty",
     }
 
     return config
-  }
+  },
 }
