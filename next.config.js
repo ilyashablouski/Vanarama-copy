204ec680af33a18a68100a9401be456ae1caebc4
iamgnlc <gianluca.agnocchetti@autorama.co.uk>
2019-12-03 15:42:03 +0000

    renamed main to app

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const withPlugins = require("next-compose-plugins")
 const withSass = require("@zeit/next-sass")
 const withImages = require("next-images")
 
-const config = require("./config/main")
+const config = require("./config/app")
 
 module.exports = withPlugins([[withSass, config.sass], withImages], config.next)
