ed1b1423fcfe5534d3fc6fb7034d58ee7d55485c
Wafiq Baugh <wafiiiq@gmail.com>
2019-12-04 12:43:43 +0000

    amend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,22 @@
 import React from "react"
 import { connect } from "react-redux"
-import * as initActions from '../redux/actions/initActions'
+import * as initActions from "../redux/actions/initActions"
 import Link from "next/link"
 
 import Header from "./shared/header"
 
 type HomeProps = {
-  init: object,
-  helloWorld: boolean
+  init: object
+  initialize: any
 }
 
-const Home = (props: HomeProps) => (
-  <div>
-    <Header />
-    {(props.helloWorld)? "Next Storefront intialized" : ":("}
-  </div>
-)
+const Home = (props: HomeProps) => {
+  return (
+    <div>
+      <Header />
+      {props.initialize.helloWorld ? "Next Storefront intialized" : ":("}
+    </div>
+  )
+}
 
-
-export default connect((state) => state, {...initActions})(Home)
+export default connect((state) => state, { ...initActions })(Home)
