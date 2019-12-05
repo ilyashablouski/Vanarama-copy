4a167bc00b83984597aab3e85bfa2db90c823add
Wafiq Baugh <wafiiiq@gmail.com>
2019-12-05 11:53:08 +0000

    redux promise added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,15 @@
 import { createStore, applyMiddleware } from "redux"
 import { composeWithDevTools } from "redux-devtools-extension"
 import thunk from "redux-thunk"
+import promise from "redux-promise"
 import reducers from "./reducers"
 
 export const initStore = (initialState = {}) => {
   return createStore(
     reducers,
     initialState,
-    process.env.NODE_ENV === "development" &&
-      composeWithDevTools(applyMiddleware(thunk)),
+    process.env.NODE_ENV === "development"
+      ? composeWithDevTools(applyMiddleware(...[thunk, promise]))
+      : applyMiddleware(thunk, promise),
   )
 }
