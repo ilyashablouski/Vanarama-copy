88b2de89b38499e518731ff69233adc2bd776d16
iamgnlc <gianluca.agnocchetti@autorama.co.uk>
2019-12-05 11:08:49 +0000

    redux wip

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,13 @@
 import { createStore, applyMiddleware } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
 import thunk from "redux-thunk"
 import reducers from "./reducers"
 
 export const initStore = (initialState = {}) => {
-  return createStore(reducers, initialState, applyMiddleware(thunk))
+  return createStore(
+    reducers,
+    initialState,
+    process.env.NODE_ENV === "development" &&
+      composeWithDevTools(applyMiddleware(thunk)),
+  )
 }
