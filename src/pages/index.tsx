b43249e74c1ccb68352af2ea8acd53a4ecfac24c
Wafiq Baugh <wafiiiq@gmail.com>
2019-12-09 16:09:33 +0000

    amend import path for test purposes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
-import * as initActions from "redux/actions/initActions"
+import * as initActions from "../redux/actions/initActions"
 
-import Header from "partials/header"
+import Header from "../partials/header"
 
 interface Initalize{
   helloWorld: boolean
