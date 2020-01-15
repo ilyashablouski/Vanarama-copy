c6f9489e9ca83e1c6d87049c55d2d1709841fbe9
Wafiq Baugh <wafiiiq@gmail.com>
2020-01-15 11:34:13 +0000

    remove form-input

--- src/components/FormInput/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const Input = ({ type, placeholder, defaultVal }) => {
-  return (
-      <input className="form--item" type={type} placeholder={placeholder} value={defaultVal} />
-  )
-}
-
-export default Input
