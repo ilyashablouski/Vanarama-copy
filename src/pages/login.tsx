d968007221a3be568532eadd14ba30e3e58e8055
Wafiq Baugh <wafiiiq@gmail.com>
2020-01-16 18:30:20 +0000

    login page importing forms

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,13 +1,26 @@
 import React, { Component } from "react"
-import { connect } from "react-redux"
+import RegisterForm from "../components/RegisterForm"
+import LoginForm from "../components/LoginForm"
 
-
-type LoginState = {
-  isLoggedIn: boolean
-}
-
+type LoginState = {}
 class LoginPage extends Component<{}, LoginState> {
-
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title h1">Login or Register</h1>
+        <section>
+          <button>Login</button>
+          <button>Register</button>
+          <div id="loginTab">
+            <LoginForm />
+          </div>
+          <div id="registerTab">
+            <RegisterForm />
+          </div>
+        </section>
+      </div>
+    )
+  }
 }
 
 export default LoginPage
