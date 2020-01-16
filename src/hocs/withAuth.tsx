508f2974230b798c61001f87756cd6954dddaed5
Wafiq Baugh <wafiiiq@gmail.com>
2020-01-16 15:51:07 +0000

    reducer renamed

--- src/hocs/withAuth.tsx
+++ src/hocs/withAuth.tsx
@@ -1,10 +1,14 @@
 import React, { Component } from "react"
 
-export const AuthHoc = (PassedComponent) => {
-  class Auth extends Component {
-    state = {}
+interface withAuthProps{
+  userAuthenticated: boolean
+}
 
+export const withAuth = <P extends object>(PassedComponent: React.ComponentType) => {
+  class Auth extends Component<P & withAuthProps> {
+    componentDidMount(){
 
+    }
 
     render() {
       return <PassedComponent />
