21ca030866835878ac797d5ef4fb9058d7d0fd1c
iamgnlc <gianluca.agnocchetti@autorama.co.uk>
2020-01-28 12:27:31 +0000

    temporary changed endpoint to aws

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -6,7 +6,8 @@ import fetch from 'isomorphic-unfetch'
 
 export const client = new ApolloClient({
     link: createHttpLink({
-      uri: "http://localhost:5300/graphql",
+      // uri: "http://localhost:5300/graphql",
+      uri: "https://79ukz5xprd.execute-api.eu-west-2.amazonaws.com/dev/graphql",
       fetch: fetch,
     }),
     cache: new InMemoryCache(),
