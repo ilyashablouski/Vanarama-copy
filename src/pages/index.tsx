6e03d904e374c77c2a34ef7006ec8615d3b2008f
Shaun <shaun@kodacell.com>
2020-01-29 09:15:45 +0000

    Fix typo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export class Home extends Component<HomeProps> {
     return (
       <div className="init">
         <Header />
-        <p>{this.props.initialize.helloWorld ? "Next Storefront intialized" : ":("}</p>
+        <p>{this.props.initialize.helloWorld ? "Next Storefront initialized" : ":("}</p>
       </div>
     )
   }
