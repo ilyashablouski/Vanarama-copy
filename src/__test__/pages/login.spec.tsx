294955eb09dfc833ea2374079fffddcfbb69073e
Wafiq Baugh <wafiiiq@gmail.com>
2020-02-18 12:11:06 +0000

    amend made

--- src/__test__/pages/login.spec.tsx
+++ src/__test__/pages/login.spec.tsx
@@ -1,19 +1,19 @@
-import LoginPage from "../../pages/login"
-import * as React from "react"
-import { shallow } from "enzyme"
+import LoginPage from '../../pages/login';
+import * as React from 'react';
+import { shallow } from 'enzyme';
 
-describe("<LoginPage /> rendering", () => {
-  let wrapper
+describe('<LoginPage /> rendering', () => {
+  let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<LoginPage />)
-  })
+    wrapper = shallow(<LoginPage />);
+  });
 
-  it("renders correctly", () => {
-    expect(wrapper).toMatchSnapshot()
-  })
+  it('renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 
   /* it("should render one <title>", () => {
     expect(wrapper.find("title")).toHaveLength(1)
   }) */
-})
+});
