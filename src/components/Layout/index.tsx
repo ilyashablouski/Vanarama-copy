f37097bd20114881573ccf3ca66f96889840ad1e
Wafiq Baugh <wafiiiq@gmail.com>
2020-02-18 11:41:20 +0000

    resolve some typeScript lint warning

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,28 +1,27 @@
 import { Grid } from 'react-raster';
 
-import Head from "../Head"
+import Head from '../Head';
 
 import '@vanarama/uibook/src/atomic/style.scss';
 
 import Router from 'next/router';
 
-const handleRouteChange = url => {
-    console.log(`App is changing to :: ${url}`)
-}
-Router.events.on('routeChangeStart', handleRouteChange)
+const handleRouteChange = (url) => {
+  console.log(`App is changing to :: ${url}`);
+};
+Router.events.on('routeChangeStart', handleRouteChange);
 
-export default ({ children, title = "Vanarama" }) => (
+export default ({ children, title = 'Vanarama' }) => (
   <>
     <Head title={title} />
 
     <Grid
       breakpoints={[0, 432, 768, 1024, 1200, 1400]}
       colspan={12}
-      control={process.env.NODE_ENV !== "production"}
-      alignX='center'
+      control={process.env.NODE_ENV !== 'production'}
+      alignX="center"
     >
-        { children }
+      {children}
     </Grid>
   </>
 );
-
