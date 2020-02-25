1209c058517c0bd4df233072e546f9a826d8761e
Wafiq Baugh <wafiiiq@gmail.com>
2020-02-25 10:37:25 +0000

    update url and pkg version

--- src/apollo/apollo.tsx
+++ src/apollo/apollo.tsx
@@ -6,7 +6,7 @@ import fetch from 'isomorphic-unfetch';
 export const client = new ApolloClient({
   ssrMode: true,
   link: createHttpLink({
-    // uri: "http://localhost:5301/graphql",
+    //uri: "http://localhost:5300/graphql",
     uri: 'https://yv8w5m1kpc.execute-api.eu-west-2.amazonaws.com',
     fetch: fetch,
   }),
