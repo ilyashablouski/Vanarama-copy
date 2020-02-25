8d29e7fd6c1fb6ecd36891486585b7ca56b45b85
Wafiq Baugh <wafiiiq@gmail.com>
2020-02-25 10:19:51 +0000

    update url

--- src/apollo/apollo.tsx
+++ src/apollo/apollo.tsx
@@ -7,7 +7,7 @@ export const client = new ApolloClient({
   ssrMode: true,
   link: createHttpLink({
     // uri: "http://localhost:5301/graphql",
-    uri: 'https://79ukz5xprd.execute-api.eu-west-2.amazonaws.com/dev/graphql',
+    uri: 'https://yv8w5m1kpc.execute-api.eu-west-2.amazonaws.com',
     fetch: fetch,
   }),
   cache: new InMemoryCache(),
