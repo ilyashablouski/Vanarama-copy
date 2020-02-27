1bc4341d835778b9b080405b6e12b8a24e9cbe25
Wafiq Baugh <wafiiiq@gmail.com>
2020-02-27 18:52:26 +0000

    remove un needed container

--- src/components/Olaf/AboutForm/AboutForm.tsx
+++ src/components/Olaf/AboutForm/AboutForm.tsx
@@ -8,7 +8,7 @@ import Input from '@vanarama/uibook/packages/ui-components/src/css/atoms/TextInp
 import Button from '@vanarama/uibook/packages/ui-components/src/css/atoms/Button/Button';
 import '@vanarama/uibook/packages/ui-components/src/css/atoms/Checkbox/Checkbox.css';
 
-import { Row, Col, Container } from 'react-grid-system';
+import { Row, Col } from 'react-grid-system';
 import { IProps, IState } from './interface';
 
 export class AboutForm extends React.Component<IProps, IState> {
@@ -79,195 +79,193 @@ export class AboutForm extends React.Component<IProps, IState> {
 
     return (
       <form onSubmit={this.handleSubmission} id="aboutForm" className="form">
-        <Container>
-          <Row>
-            <Col>
-              <label>Title</label>
-              <Select
-                name="title"
-                onChange={this.handleInputChange}
-                options={this.state.allDropDowns.titles || {}}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Input
-                label="First Name"
-                handleChange={this.handleInputChange}
-                type="text"
-                name="firstName"
-                value={firstName}
-                id="aboutInputFirstName"
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Input
-                label="Last Name"
-                handleChange={this.handleInputChange}
-                type="text"
-                name="lastName"
-                value={lastName}
-                id="aboutInputLastName"
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Input
-                label="Email"
-                handleChange={this.handleInputChange}
-                type="email"
-                name="email"
-                value={email}
-                id="aboutInputEmail"
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Input
-                label="Phone Number"
-                handleChange={this.handleInputChange}
-                type="tel"
-                name="mobile"
-                value={mobile}
-                id="aboutInputPhoneNumber"
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col sm={5}>
-              <label>Date of Birth</label>
-              <Row>
-                <Col sm={4}>
-                  <Select
-                    name="dayOfBirth"
-                    onChange={this.handleInputChange}
-                    options={{
-                      data: [...Array(31)].map((_, i) => ({
-                        value: i + 1,
-                      })),
-                    }}
-                  />
-                </Col>
-                <Col sm={4}>
-                  <Select
-                    name="monthOfBirth"
-                    onChange={this.handleInputChange}
-                    options={{
-                      data: months.map((month) => ({
-                        value: month,
-                      })),
-                    }}
-                  />
-                </Col>
-                <Col sm={4}>
-                  <Select
-                    name="yearOfBirth"
-                    onChange={this.handleInputChange}
-                    options={{
-                      data: years.map((year) => ({
-                        value: year,
-                      })),
-                    }}
-                  />
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <label>Country of Birth</label>
-              <Select
-                name="countryOfBirth"
-                onChange={this.handleInputChange}
-                options={this.state.allDropDowns.countries || {}}
-                id={'aboutInputCOB'}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <label>Nationality</label>
-              <Select
-                name="nationality"
-                onChange={this.handleInputChange}
-                options={this.state.allDropDowns.nationalities || {}}
-                id={'aboutInputNationality'}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <label>Marital Status</label>
-              <Select
-                name="maritalStatus"
-                onChange={this.handleInputChange}
-                options={this.state.allDropDowns.maritalStatuses || {}}
-                id={'aboutInputMarStatus'}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <label>No. of Dependants</label>
-              <Select
-                name="dependants"
-                onChange={this.handleInputChange}
-                options={this.state.allDropDowns.noOfDependants || {}}
-                id={'aboutInputMarDependants'}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <label>No. of Adults in Household</label>
-              <Select
-                name="adultsInHousehold"
-                onChange={this.handleInputChange}
-                options={this.state.allDropDowns.noOfAdultsInHousehold || {}}
-                id={'aboutInputAdultsHoushold'}
-              />
-            </Col>
-          </Row>
-          <br />
-          <div role="presentation">
-            <div className="Form__item -Checkbox__wrapper">
-              <input
-                onChange={this.handleInputChange}
-                className="Checkbox"
-                type="checkbox"
-                name="consent"
-                id={'aboutInputConsent'}
-              />
-              <label className="Checkbox__label" htmlFor={'aboutInputConsent'}>
-                <span className="Text -secondary">
-                  I wish to receive emails and SMS messages for updates on the
-                  latest deals, offers and promotions.
-                </span>
-              </label>
-            </div>
-            <div className="Form__item -Checkbox__wrapper">
-              <input
-                onChange={this.handleInputChange}
-                className="Checkbox"
-                type="checkbox"
-                name="termsAndCons"
-                id={'aboutInputT&C'}
-              />
-              <label className="Checkbox__label" htmlFor={'aboutInputT&C'}>
-                <span className="Text -secondary">
-                  agree to the terms and conditions.
-                </span>
-              </label>
-            </div>
+        <Row>
+          <Col>
+            <label>Title</label>
+            <Select
+              name="title"
+              onChange={this.handleInputChange}
+              options={this.state.allDropDowns.titles || {}}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Input
+              label="First Name"
+              handleChange={this.handleInputChange}
+              type="text"
+              name="firstName"
+              value={firstName}
+              id="aboutInputFirstName"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Input
+              label="Last Name"
+              handleChange={this.handleInputChange}
+              type="text"
+              name="lastName"
+              value={lastName}
+              id="aboutInputLastName"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Input
+              label="Email"
+              handleChange={this.handleInputChange}
+              type="email"
+              name="email"
+              value={email}
+              id="aboutInputEmail"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Input
+              label="Phone Number"
+              handleChange={this.handleInputChange}
+              type="tel"
+              name="mobile"
+              value={mobile}
+              id="aboutInputPhoneNumber"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={5}>
+            <label>Date of Birth</label>
+            <Row>
+              <Col sm={4}>
+                <Select
+                  name="dayOfBirth"
+                  onChange={this.handleInputChange}
+                  options={{
+                    data: [...Array(31)].map((_, i) => ({
+                      value: i + 1,
+                    })),
+                  }}
+                />
+              </Col>
+              <Col sm={4}>
+                <Select
+                  name="monthOfBirth"
+                  onChange={this.handleInputChange}
+                  options={{
+                    data: months.map((month) => ({
+                      value: month,
+                    })),
+                  }}
+                />
+              </Col>
+              <Col sm={4}>
+                <Select
+                  name="yearOfBirth"
+                  onChange={this.handleInputChange}
+                  options={{
+                    data: years.map((year) => ({
+                      value: year,
+                    })),
+                  }}
+                />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <label>Country of Birth</label>
+            <Select
+              name="countryOfBirth"
+              onChange={this.handleInputChange}
+              options={this.state.allDropDowns.countries || {}}
+              id={'aboutInputCOB'}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <label>Nationality</label>
+            <Select
+              name="nationality"
+              onChange={this.handleInputChange}
+              options={this.state.allDropDowns.nationalities || {}}
+              id={'aboutInputNationality'}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <label>Marital Status</label>
+            <Select
+              name="maritalStatus"
+              onChange={this.handleInputChange}
+              options={this.state.allDropDowns.maritalStatuses || {}}
+              id={'aboutInputMarStatus'}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <label>No. of Dependants</label>
+            <Select
+              name="dependants"
+              onChange={this.handleInputChange}
+              options={this.state.allDropDowns.noOfDependants || {}}
+              id={'aboutInputMarDependants'}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <label>No. of Adults in Household</label>
+            <Select
+              name="adultsInHousehold"
+              onChange={this.handleInputChange}
+              options={this.state.allDropDowns.noOfAdultsInHousehold || {}}
+              id={'aboutInputAdultsHoushold'}
+            />
+          </Col>
+        </Row>
+        <br />
+        <div role="presentation">
+          <div className="Form__item -Checkbox__wrapper">
+            <input
+              onChange={this.handleInputChange}
+              className="Checkbox"
+              type="checkbox"
+              name="consent"
+              id={'aboutInputConsent'}
+            />
+            <label className="Checkbox__label" htmlFor={'aboutInputConsent'}>
+              <span className="Text -secondary">
+                I wish to receive emails and SMS messages for updates on the
+                latest deals, offers and promotions.
+              </span>
+            </label>
           </div>
-          <br />
-          <Button type="submit" label="Continue" color="primary" />
-        </Container>
+          <div className="Form__item -Checkbox__wrapper">
+            <input
+              onChange={this.handleInputChange}
+              className="Checkbox"
+              type="checkbox"
+              name="termsAndCons"
+              id={'aboutInputT&C'}
+            />
+            <label className="Checkbox__label" htmlFor={'aboutInputT&C'}>
+              <span className="Text -secondary">
+                agree to the terms and conditions.
+              </span>
+            </label>
+          </div>
+        </div>
+        <br />
+        <Button type="submit" label="Continue" color="primary" />
       </form>
     );
   }
