4099b9be3ec137dea04d9f6222b90181866bc2ab
Umer <holypulse2@gmail.com>
2020-03-02 15:22:04 +0000

    Login components

---
+++ src/components/account/login/Login.tsx
@@ -0,0 +1,97 @@
+import React, { ChangeEvent } from 'react';
+import { Row, Col } from 'react-grid-system';
+import localForage from 'localforage';
+import Input from '@vanarama/uibook/packages/ui-components/src/css/atoms/TextInput';
+import Button from '@vanarama/uibook/packages/ui-components/src/css/atoms/Button/Button';
+import Link from '@vanarama/uibook/packages/ui-components/src/css/atoms/Link';
+import '@vanarama/uibook/packages/ui-components/src/css/theme/helpers/classes.css';
+import { LoginProps, LoginState } from './interfaces';
+ 
+class Login extends React.Component<LoginProps, LoginState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      token: null,
+    };
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.props.token) {
+      localForage.setItem('va-token', this.props.token);
+    }
+  }
+
+  handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { email, password } = this.state;
+
+    this.props.login(email, password);
+  };
+
+   handleInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    e.preventDefault();
+    const { name, value } = e.currentTarget;
+
+    if (Object.keys(this.state).includes(name)) {
+      this.setState(
+        (prevState) => ({ ...prevState, [name]: value })
+      );
+    }
+  };
+
+  render() {
+    const { email, password } = this.state;
+
+    return (
+      <section>
+        <form onSubmit={this.handleSubmit} id="loginForm" className="form">
+          <Row style={{ marginBottom: '16px'}}>
+            <Col>
+              <Input
+                id="email"
+                label="Your Email"
+                type="text"
+                name="email"
+                value={ email }
+                handleChange={this.handleInputChange}
+              />
+            </Col>
+          </Row>  
+          <Row style={{ marginBottom: '16px'}}>
+            <Col>
+              <Input
+                id="password"
+                label="Your Password"
+                type="password"
+                name="password"
+                value={ password }
+                handleChange={this.handleInputChange}
+              />
+            </Col>
+          </Row>  
+          <Row style={{ marginBottom: '16px'}}>
+            <Col>
+              <Link>forgot your password?</Link>
+            </Col>
+          </Row>  
+          <Row style={{ marginBottom: '16px'}}>
+            <Col>
+              <Button type="submit" label="Login" color="primary" />
+            </Col>
+          </Row>  
+          <Row style={{ marginBottom: '16px'}}>
+            <Col>
+              { this.props.authenticated ? <p id="loginStatus">Login Success</p> : null}
+              { this.props.authenticated === false ? <p id="loginFailure">Login Failed</p>: null}
+            </Col>
+          </Row>  
+        </form>
+      </section>
+    );
+  }
+}
+
+export default Login;
