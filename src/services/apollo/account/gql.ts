c026ba38671e78a403c8e9354fb2a8bdce1776ca
Umer <holypulse2@gmail.com>
2020-03-02 14:42:01 +0000

    Apollo api setup

---
+++ src/services/apollo/account/gql.ts
@@ -0,0 +1,19 @@
+import { gql } from 'apollo-boost';
+
+export const LOGIN = gql`
+  mutation CreateRegisteredUser($email: String!, $pw: String!) {
+    login(username: $email, password: $pw)
+  }
+`;
+
+export const REQUEST_NEW_PASSWORD = gql`
+  mutation RequestNewPassword($email: String!) {
+    passwordReset(username: $email)
+  }
+`;
+
+export const CREATE_NEW_PASSOWRD = gql`
+  mutation CreateNewPassword($code: String!, $email: String!, $pw: String!) {
+    passwordConfirm(verificationCode: $code, username: $email, password: $pw)
+  }
+`;
