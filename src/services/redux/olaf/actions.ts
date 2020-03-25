755e1644e7d4d9dc99fe870fa5f0210695d4c7eb
Wafiq Baugh <wafiiiq@gmail.com>
2020-03-25 12:29:02 +0000

    correct eslint annoyance

--- src/services/redux/olaf/actions.ts
+++ src/services/redux/olaf/actions.ts
@@ -1,6 +1,7 @@
 import { TMP_OLAF_DATA } from './types';
 
-export const captchaOlafData = (pageRef: string, data: {}) => (dispatch) => {
+// eslint-disable-next-line import/prefer-default-export
+export const captchaOlafData = (pageRef: string, data: {}) => dispatch => {
   dispatch({
     type: TMP_OLAF_DATA,
     payload: { pageRef, data },
