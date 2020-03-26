9e137b8e4c9ec346ace50295c9d77c908f6b967f
Chris Head <chead2344@gmail.com>
2020-03-26 09:49:47 +0000

    Minor styling changes to match designs and updated copy for terms & conditions message

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,9 +1,10 @@
 import { useMutation } from '@apollo/react-hooks';
+import ChevronForwardSharpIcon from '@vanarama/uibook/src/assets/icons/ChevronForwardSharp';
 import Button from '@vanarama/uibook/src/components/atoms/button';
 import Details from '@vanarama/uibook/src/components/atoms/details';
+import Link from '@vanarama/uibook/src/components/atoms/link';
 import Text from '@vanarama/uibook/src/components/atoms/text';
 import TextInput from '@vanarama/uibook/src/components/atoms/textinput';
-import Formgroup from '@vanarama/uibook/src/components/molecules/formgroup';
 import { gql } from 'apollo-boost';
 import React from 'react';
 import { Controller, useForm } from 'react-hook-form';
@@ -51,81 +52,90 @@ const RegisterForm: React.FC = () => {
 
   return (
     <form id="register-form" className="form" onSubmit={handleSubmit(onSubmit)}>
-      <Formgroup>
-        <Controller
-          name="email"
-          type="email"
-          as={TextInput}
-          control={control}
-          label="Your Email"
-          invalid={errors.email && errors.email.message}
-          rules={{
-            required: {
-              value: true,
-              message: 'Your Email is required',
-            },
-            pattern: {
-              value: EMAIL_REGEX,
-              message: 'Invalid email address',
-            },
-          }}
-        />
-      </Formgroup>
-      <Formgroup>
-        <Controller
-          name="password"
-          type="password"
-          as={TextInput}
-          control={control}
-          invalid={errors.password && errors.password.message}
-          label="Your Password"
-          rules={{
-            required: {
-              value: true,
-              message: 'Your Password is required',
-            },
-            pattern: {
-              value: PASSWORD_REGEX,
-              message: 'Your Password does not meet the requirements',
-            },
-          }}
-        />
-      </Formgroup>
+      <Controller
+        name="email"
+        type="email"
+        as={TextInput}
+        control={control}
+        label="Your Email"
+        invalid={errors.email && errors.email.message}
+        rules={{
+          required: {
+            value: true,
+            message: 'Your Email is required',
+          },
+          pattern: {
+            value: EMAIL_REGEX,
+            message: 'Invalid email address',
+          },
+        }}
+      />
+      <Controller
+        name="password"
+        type="password"
+        as={TextInput}
+        control={control}
+        invalid={errors.password && errors.password.message}
+        label="Your Password"
+        rules={{
+          required: {
+            value: true,
+            message: 'Your Password is required',
+          },
+          pattern: {
+            value: PASSWORD_REGEX,
+            message: 'Your Password does not meet the requirements',
+          },
+        }}
+      />
       <Details
         summary="Password Requirements"
         content="Must be 8 characters long, contain at least 1 number, contain uppercase letters and contain lowercase letters."
       />
-      <Formgroup>
-        <Controller
-          name="confirmPassword"
-          type="password"
-          as={TextInput}
-          control={control}
-          invalid={errors.confirmPassword && errors.confirmPassword.message}
-          label="Repeat Password"
-          rules={{
-            validate: value => {
-              return watch('password') !== value
-                ? 'Repeat Password does not match'
-                : null;
-            },
-            required: {
-              value: true,
-              message: 'Repeat Password is required',
-            },
-          }}
-        />
-      </Formgroup>
+      <Controller
+        name="confirmPassword"
+        type="password"
+        as={TextInput}
+        control={control}
+        invalid={errors.confirmPassword && errors.confirmPassword.message}
+        label="Repeat Password"
+        rules={{
+          validate: value => {
+            return watch('password') !== value
+              ? 'Repeat Password does not match'
+              : null;
+          },
+          required: {
+            value: true,
+            message: 'Repeat Password is required',
+          },
+        }}
+      />
       <Text tag="p" color="darker" size="xsmall">
-        Terms and conditions agreement text and link
+        By creating your account, you agree to our{' '}
+        <Link href="https://www.motorama.com/terms-conditions" size="xsmall">
+          Terms and Conditions
+        </Link>{' '}
+        and{' '}
+        <Link
+          href="https://www.motorama.com/cookie-privacy-policy"
+          size="xsmall"
+        >
+          Privacy Policy
+        </Link>
+        .
       </Text>
-      <Formgroup>
-        {loading ? (
-          <Button type="submit" label="Loading..." disabled color="primary" />
-        ) : (
-          <Button type="submit" label="Register" color="primary" />
-        )}
-      </Formgroup>
+      {loading ? (
+        <Button type="submit" label="Loading..." disabled color="primary" />
+      ) : (
+        <Button
+          type="submit"
+          label="Register"
+          icon={<ChevronForwardSharpIcon />}
+          iconPosition="after"
+          color="primary"
+        />
+      )}
     </form>
   );
 };
