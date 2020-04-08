0a2771c723b3b12464a3a66487e0b79bf8b51a24
Wafiq Baugh <wafiiiq@gmail.com>
2020-04-08 14:54:37 +0100

    add when

--- src/components/olaf/about-form/AboutForm.validation.ts
+++ src/components/olaf/about-form/AboutForm.validation.ts
@@ -29,56 +29,74 @@ function ageValidator() {
   return error ? createError({ message: error, path }) : true;
 }
 
-const ValidationSchema = yup.object().shape<IAboutFormValues>({
-  title: yup.string().required('Please select a title'),
-  firstName: yup
-    .string()
-    .required(reqMsg('first name'))
-    .min(
-      2,
-      'Oops, this name’s too short. Please make it longer than 2 characters',
-    )
-    .max(50, 'Oops, this name’s too long. Please keep it to 50 characters'),
-  lastName: yup
-    .string()
-    .required(reqMsg('last name'))
-    .min(
-      2,
-      'Oops, this name’s too short. Please make it longer than 2 characters',
-    )
-    .max(50, 'Oops, this name’s too long. Please keep it to 50 characters'),
-  email: yup
-    .string()
-    .required(reqMsg('Email'))
-    .email('Oops, this email address is invalid'),
-  mobile: yup
-    .string()
-    .matches(
-      /^([+]\d{2})?\d{11,12}$/,
-      'Please enter mobile number without spaces or hyphens',
-    )
-    .required(reqMsg('Mobile')),
-  dayOfBirth: yup
-    .string()
-    .required('Please complete your date of birth')
-    .test('age', 'Invalid age', ageValidator),
-  monthOfBirth: yup
-    .string()
-    .required('Please complete your date of birth')
-    .test('age', 'Invalid age', ageValidator),
-  yearOfBirth: yup
-    .string()
-    .required('Please complete your date of birth')
-    .test('age', 'Invalid age', ageValidator),
-  countryOfBirth: yup.string().required(reqMsg('country of birth')),
-  nationality: yup.string().required(reqMsg('nationality')),
-  maritalStatus: yup.string().required(reqMsg('marital status')),
-  dependants: yup.string().required('Please enter number of dependants'),
-  adultsInHousehold: yup.string().required('Please enter adults in household'),
-  consent: yup.boolean().notRequired(),
-  termsAndCons: yup
-    .boolean()
-    .oneOf([true], 'The terms and conditions must be accepted.'),
-});
+const ValidationSchema = yup.object().shape<IAboutFormValues>(
+  {
+    title: yup.string().required('Please select a title'),
+    firstName: yup
+      .string()
+      .required(reqMsg('first name'))
+      .min(
+        2,
+        'Oops, this name’s too short. Please make it longer than 2 characters',
+      )
+      .max(50, 'Oops, this name’s too long. Please keep it to 50 characters'),
+    lastName: yup
+      .string()
+      .required(reqMsg('last name'))
+      .min(
+        2,
+        'Oops, this name’s too short. Please make it longer than 2 characters',
+      )
+      .max(50, 'Oops, this name’s too long. Please keep it to 50 characters'),
+    email: yup
+      .string()
+      .required(reqMsg('Email'))
+      .email('Oops, this email address is invalid'),
+    mobile: yup
+      .string()
+      .matches(
+        /^([+]\d{2})?\d{11,12}$/,
+        'Please enter mobile number without spaces or hyphens',
+      )
+      .required(reqMsg('Mobile')),
+    dayOfBirth: yup
+      .string()
+      .required('Please complete your date of birth')
+      .when(['monthOfBirth', 'yearOfBirth'], {
+        is: (monthOfBirth, yearOfBirth) => !!monthOfBirth && !!yearOfBirth,
+        then: yup.string().test('age', 'Invalid age', ageValidator),
+      }),
+    monthOfBirth: yup
+      .string()
+      .required('Please complete your date of birth')
+      .when(['dayOfBirth', 'yearOfBirth'], {
+        is: (dayOfBirth, yearOfBirth) => !!dayOfBirth && !!yearOfBirth,
+        then: yup.string().test('age', 'Invalid age', ageValidator),
+      }),
+    yearOfBirth: yup
+      .string()
+      .required('Please complete your date of birth')
+      .when(['dayOfBirth', 'monthOfBirth'], {
+        is: (dayOfBirth, monthOfBirth) => !dayOfBirth && !monthOfBirth,
+        then: yup.string().test('age', 'Invalid age', ageValidator),
+      }),
+    countryOfBirth: yup.string().required(reqMsg('country of birth')),
+    nationality: yup.string().required(reqMsg('nationality')),
+    maritalStatus: yup.string().required(reqMsg('marital status')),
+    dependants: yup.string().required('Please enter number of dependants'),
+    adultsInHousehold: yup
+      .string()
+      .required('Please enter adults in household'),
+    consent: yup.boolean().notRequired(),
+    termsAndCons: yup
+      .boolean()
+      .oneOf([true], 'The terms and conditions must be accepted.'),
+  },
+  [
+    ['dayOfBirth', 'monthOfBirth'],
+    ['dayOfBirth', 'yearOfBirth'],
+    ['monthOfBirth', 'yearOfBirth'],
+  ],
+);
 
 export default ValidationSchema;
