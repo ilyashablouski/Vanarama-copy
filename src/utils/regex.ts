66f9b2c986d6cb53548283454dd81d5a6466c190
Wafiq Baugh <wafiiiq@gmail.com>
2020-04-20 10:13:20 +0100

    add uk mobile just incase

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -3,3 +3,5 @@ export const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 export const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 
 export const WORLDWIDE_MOBILE_REGEX = /^((\+[0-9]+(0|\(0\)|\s0\s|\s)?)|0)[0-9]+\d{3}(\s)?\d{6}/;
+
+export const UK_MOBILE_REGEX = /^((\+44(0|\(0\)|\s0\s|\s)?)|0)7\d{3}(\s)?\d{6}/;
