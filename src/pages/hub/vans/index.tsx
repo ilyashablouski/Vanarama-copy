5b3aef5e3ff6dd71a4fbac44e5cfd081c747778d
Wafiq Baugh <wafiiiq@gmail.com>
2020-05-01 12:27:33 +0100

    removed duplicate sections

--- src/pages/hub/vans/index.tsx
+++ src/pages/hub/vans/index.tsx
@@ -451,60 +451,54 @@ const VansPage: NextPage = () => (
       </div>
     </section>
 
-    <section className="league">
-      <League altText="vanarama national league" />
-    </section>
-    <section className="logo-row">
-      <div className="container">
-        <LogoRow
-          urls={[
-            {
-              label: 'bbc',
-              href:
-                'https://www.vanarama.com/Assets/images-optimised/home/featured/bbc.png',
-            },
-            {
-              label: 'btsport',
-              href:
-                'https://www.vanarama.com/Assets/images-optimised/home/featured/btsport.png',
-            },
-            {
-              label: 'dailymirror',
-              href:
-                'https://www.vanarama.com/Assets/images-optimised/home/featured/dailymail.png',
-            },
-            {
-              label: 'itv',
-              href:
-                'https://www.vanarama.com/Assets/images-optimised/home/featured/itv.png',
-            },
-            {
-              label: 'metro',
-              href:
-                'https://www.vanarama.com/Assets/images-optimised/home/featured/metro.png',
-            },
-            {
-              label: 'thesun',
-              href:
-                'https://www.vanarama.com/Assets/images-optimised/home/featured/thesun.png',
-            },
-            {
-              label: 'sky',
-              href:
-                'https://www.vanarama.com/Assets/images-optimised/home/featured/sky.png',
-            },
-            {
-              label: 'thetelegraph',
-              href:
-                'https://www.vanarama.com/Assets/images-optimised/home/featured/thetelegraph.png',
-            },
-          ]}
-        />
-      </div>
-    </section>
-    <section className="trust-pilot">
-      <TrustPilot src="https://widget.trustpilot.com/trustboxes/53aa8912dec7e10d38f59f36/index.html?templateId=53aa8912dec7e10d38f59f36&amp;businessunitId=594a982f0000ff0005a50d80#locale=en-GB&amp;styleHeight=130px&amp;styleWidth=100%25&amp;theme=light&amp;stars=4%2C5&amp;schemaType=Organization" />
-    </section>
+    <League altText="vanarama national league" />
+
+    <LogoRow
+      urls={[
+        {
+          label: 'bbc',
+          href:
+            'https://www.vanarama.com/Assets/images-optimised/home/featured/bbc.png',
+        },
+        {
+          label: 'btsport',
+          href:
+            'https://www.vanarama.com/Assets/images-optimised/home/featured/btsport.png',
+        },
+        {
+          label: 'dailymirror',
+          href:
+            'https://www.vanarama.com/Assets/images-optimised/home/featured/dailymail.png',
+        },
+        {
+          label: 'itv',
+          href:
+            'https://www.vanarama.com/Assets/images-optimised/home/featured/itv.png',
+        },
+        {
+          label: 'metro',
+          href:
+            'https://www.vanarama.com/Assets/images-optimised/home/featured/metro.png',
+        },
+        {
+          label: 'thesun',
+          href:
+            'https://www.vanarama.com/Assets/images-optimised/home/featured/thesun.png',
+        },
+        {
+          label: 'sky',
+          href:
+            'https://www.vanarama.com/Assets/images-optimised/home/featured/sky.png',
+        },
+        {
+          label: 'thetelegraph',
+          href:
+            'https://www.vanarama.com/Assets/images-optimised/home/featured/thetelegraph.png',
+        },
+      ]}
+    />
+
+    <TrustPilot src="https://widget.trustpilot.com/trustboxes/53aa8912dec7e10d38f59f36/index.html?templateId=53aa8912dec7e10d38f59f36&amp;businessunitId=594a982f0000ff0005a50d80#locale=en-GB&amp;styleHeight=130px&amp;styleWidth=100%25&amp;theme=light&amp;stars=4%2C5&amp;schemaType=Organization" />
   </main>
 );
 
