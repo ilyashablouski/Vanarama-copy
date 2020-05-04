345760a8ec84d508d3be0cf68f6bc9c440425f44
Umer <holypulse2@gmail.com>
2020-05-04 14:02:34 +0100

    Email already exists

--- src/components/RegisterForm/RegisterForm.spec.tsx
+++ src/components/RegisterForm/RegisterForm.spec.tsx
@@ -1,15 +1,47 @@
-import { fireEvent, render, waitFor } from '@testing-library/react';
+import { fireEvent, render, waitFor, act } from '@testing-library/react';
 import React from 'react';
 import RegisterForm from './RegisterForm';
+
+const jestMock = (emailAlreadyExists: boolean) => {
+  return jest.fn().mockReturnValue({ data: { emailAlreadyExists } });
+};
+
+const renderComponent = (
+  onSubmit: jest.Mock<any, any>,
+  emailAlreadyExists: jest.Mock<any, any>,
+) => {
+  return render(
+    <RegisterForm
+      onSubmit={onSubmit}
+      onEmailAlreadyExists={emailAlreadyExists}
+    />,
+  );
+};
+
+const fillTextandBlur = async (
+  getByLabelText: any,
+  labelText: string,
+  email: string,
+) => {
+  fireEvent.input(getByLabelText(labelText), {
+    target: { value: email },
+  });
+
+  await act(async () => {
+    fireEvent.blur(getByLabelText(labelText));
+  });
+};
 
 describe('<RegisterForm />', () => {
   it('should call `onSubmit` if there are no validation errors', async () => {
     // ARRANGE
     const onSubmit = jest.fn();
+    const emailAlreadyExists = jestMock(false);
 
     // ACT
-    const { getByLabelText, getByText } = render(
-      <RegisterForm onSubmit={onSubmit} />,
+    const { getByLabelText, getByText } = renderComponent(
+      onSubmit,
+      emailAlreadyExists,
     );
 
     // Set the email address
@@ -38,7 +70,7 @@ describe('<RegisterForm />', () => {
     const onSubmit = jest.fn();
 
     // ACT
-    const { getByText } = render(<RegisterForm onSubmit={onSubmit} />);
+    const { getByText } = renderComponent(onSubmit, jest.fn());
 
     fireEvent.click(getByText('Register'));
 
@@ -56,9 +88,7 @@ describe('<RegisterForm />', () => {
     const onSubmit = jest.fn();
 
     // ACT
-    const { getByLabelText, getByText } = render(
-      <RegisterForm onSubmit={onSubmit} />,
-    );
+    const { getByLabelText, getByText } = renderComponent(onSubmit, jest.fn());
 
     // Set the email address
     fireEvent.input(getByLabelText('Your Email'), {
@@ -90,9 +120,7 @@ describe('<RegisterForm />', () => {
     const onSubmit = jest.fn();
 
     // ACT
-    const { getByLabelText, getByText } = render(
-      <RegisterForm onSubmit={onSubmit} />,
-    );
+    const { getByLabelText, getByText } = renderComponent(onSubmit, jest.fn());
 
     // Set the email address
     fireEvent.input(getByLabelText('Your Email'), {
@@ -115,5 +143,46 @@ describe('<RegisterForm />', () => {
     await waitFor(() =>
       expect(getByText('Repeat Password does not match')).toBeVisible(),
     );
+  });
+
+  describe('Email Already Exists', () => {
+    let onSubmit: jest.Mock<any, any>;
+    const labelText = 'Your Email';
+    const errorMessage = 'Email Already Exists';
+    const email = 'test@test.com';
+
+    beforeEach(() => {
+      onSubmit = jest.fn();
+    });
+
+    afterEach(() => {
+      onSubmit.mockReset();
+    });
+
+    it('should show validation email error for existing email', async () => {
+      const emailAlreadyExists = jestMock(true);
+      const { getByLabelText, getByText } = renderComponent(
+        onSubmit,
+        emailAlreadyExists,
+      );
+
+      fillTextandBlur(getByLabelText, labelText, email);
+
+      await waitFor(() => expect(getByText(errorMessage)).toBeVisible());
+    });
+
+    it('should not show validation email error for new email', async () => {
+      const emailAlreadyExists = jestMock(false);
+      const { getByLabelText, queryByText } = renderComponent(
+        onSubmit,
+        emailAlreadyExists,
+      );
+
+      fillTextandBlur(getByLabelText, labelText, email);
+
+      await waitFor(() =>
+        expect(queryByText(errorMessage)).not.toBeInTheDocument(),
+      );
+    });
   });
 });
