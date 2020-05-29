996b5da5c5c08f2bf51a149872c0e9a3e1a4d1f1
Theo Andresier <theo@techamigos.co>
2020-05-29 14:20:09 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ APP=${3}
 REGION=${4}
 CURRENTBRANCH=${5}
 
-export API_KEY="$(aws ssm get-parameter --name "/$ENV/$STACK/$APP/API_KEY" --region $REGION --with-decryption | jq -r ".Parameter.Value")"
-export API_URL="$(aws ssm get-parameter --name "/$ENV/$STACK/$APP/API_URL" --region $REGION --with-decryption | jq -r ".Parameter.Value")"
+export API_KEY = "$(aws ssm get-parameter --name "/$ENV/$STACK/$APP/API_KEY" --region $REGION --with-decryption | jq -r ".Parameter.Value")"
+export API_URL = "$(aws ssm get-parameter --name "/$ENV/$STACK/$APP/API_URL" --region $REGION --with-decryption | jq -r ".Parameter.Value")"
