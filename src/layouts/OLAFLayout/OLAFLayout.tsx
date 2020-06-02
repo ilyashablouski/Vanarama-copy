d1a9411d4a1b576b9fe4eed9499dbc4ce7c9b586
Chris Head <chead2344@gmail.com>
2020-06-02 16:00:56 +0100

    fix: fix the styling of the OLAFAside on mobile viewports (#208)

--- src/layouts/OLAFLayout/OLAFLayout.tsx
+++ src/layouts/OLAFLayout/OLAFLayout.tsx
@@ -9,38 +9,41 @@ interface IProps {
   hideProgress?: boolean;
 }
 
-const OlafAsideComponent = () => (
-  <OlafAside
-    price={209}
-    imageSrc="https://res.cloudinary.com/diun8mklf/image/upload/c_fill,g_center,h_425,q_auto:best,w_800/v1581538983/cars/KiaeNiro0219_j7on5z.jpg"
-    rating={4.5}
-    subtitle="1.4T ecoTEC Elite Nav 5dr"
-    title="FIAT 500 Hatchback"
-    initailRental="£815.70 (inc VAT)"
-    contractLength="60 months"
-    annualMileage="6000 miles"
-    fuel="Petrol"
-    transmission="Manual"
-    color="Solid - Polar white"
-    trim="Cloth - Black"
-  />
-);
-
 const OLAFLayout: React.FC<IProps> = ({ children, hideProgress }) => (
   <>
     {!hideProgress && (
       <div className="row:progress">
         <OLAFProgressIndicator />
         <OlafAsideToggle>
-          <OlafAsideComponent />
+          <OLAFAsideWrapper />
         </OlafAsideToggle>
       </div>
     )}
     <div className="row:olaf">
       {children}
-      <OlafAsideComponent />
+      <OLAFAsideWrapper className="-vp-min:small" />
     </div>
   </>
 );
+
+function OLAFAsideWrapper({ className }: { className?: string }) {
+  return (
+    <OlafAside
+      annualMileage="6000 miles"
+      className={className}
+      color="Solid - Polar white"
+      contractLength="60 months"
+      fuel="Petrol"
+      imageSrc="https://res.cloudinary.com/diun8mklf/image/upload/c_fill,g_center,h_425,q_auto:best,w_800/v1581538983/cars/KiaeNiro0219_j7on5z.jpg"
+      initailRental="£815.70 (inc VAT)"
+      price={209}
+      rating={4.5}
+      subtitle="1.4T ecoTEC Elite Nav 5dr"
+      title="FIAT 500 Hatchback"
+      transmission="Manual"
+      trim="Cloth - Black"
+    />
+  );
+}
 
 export default OLAFLayout;
