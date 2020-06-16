fb165b9741cdd81fa160dc54e56b0a713567d464
Aliaksei Douhal <kcafmil@gmail.com>
2020-06-16 15:19:32 +0300

    fixed lint

--- src/containers/SearchPodContainer/SearchPodContainer.tsx
+++ src/containers/SearchPodContainer/SearchPodContainer.tsx
@@ -170,8 +170,9 @@ const SearchPodContainer = () => {
         values[`type${tabType}`] &&
         !(isCarTab && values[`model${tabType}`])
       ) {
-        queryTypePart = `bodyType=${values[`type${tabType}`].split(' ')
-        .join('')}`;
+        queryTypePart = `bodyType=${values[`type${tabType}`]
+          .split(' ')
+          .join('')}`;
         queryPart += `${queryTypePart}${values[`type${tabType}`] &&
           values[`budget${tabType}`] &&
           (!(isCarTab && values[`model${tabType}`]) || '') &&
