7fd5eec3f3c72a2deceeb95f4cf3963f67a454ea
Aliaksei Douhal <kcafmil@gmail.com>
2020-06-19 10:57:59 +0300

    fixed query variable

--- src/containers/SearchPodContainer/__tests__/SearchPodContainer.spec.tsx
+++ src/containers/SearchPodContainer/__tests__/SearchPodContainer.spec.tsx
@@ -49,7 +49,7 @@ const mocks: MockedResponse[] = [
       variables: {
         vehicleTypes: ['LCV'],
         manufacturerName: '',
-        modelName: '',
+        rangeName: '',
       },
     },
     result: () => {
