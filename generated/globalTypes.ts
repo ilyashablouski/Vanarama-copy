d5cd83c9ebe509790e88e9877e6edea29b302637
Wafiq Baugh <wafiiiq@gmail.com>
2020-06-22 13:34:31 +0100

    add rowText

--- generated/globalTypes.ts
+++ generated/globalTypes.ts
@@ -165,7 +165,6 @@ export interface IncomeAndExpenseInputObject {
  */
 export interface PersonInputObject {
   about?: string | null;
-  address?: AddressInputObject | null;
   company?: CompanyInputObject | null;
   countryOfBirth?: string | null;
   dateOfBirth?: any | null;
@@ -189,7 +188,7 @@ export interface PersonInputObject {
   profilingConsent?: boolean | null;
   role?: RoleInputObject | null;
   smsConsent?: boolean | null;
-  telephoneNumber?: TelephoneNumberInputObject | null;
+  telephoneNumbers?: TelephoneNumberInputObject[] | null;
   termsAndConditions?: boolean | null;
   title?: string | null;
   tradingName?: string | null;
