2cd42d75add5cc2089f1c320854f0b79a2d4117e
AntonAdamkovich <anton.adamkovich@gmail.com>
2020-07-01 18:10:06 +0300

    Add intergration with personByToken quesry

--- src/pages/olaf/about/index.tsx
+++ src/pages/olaf/about/index.tsx
@@ -1,15 +1,35 @@
 import { useState } from 'react';
 import { getDataFromTree } from '@apollo/react-ssr';
+import { gql, useLazyQuery } from '@apollo/client';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import Button from '@vanarama/uibook/lib/components/atoms/button';
 import Heading from '@vanarama/uibook/lib/components/atoms/heading';
 import Text from '@vanarama/uibook/lib/components/atoms/text';
+import * as toast from '@vanarama/uibook/lib/components/atoms/toast/Toast';
 import AboutFormContainer from '../../../containers/AboutFormContainer/AboutFormContainer';
 import LoginFormContainer from '../../../containers/LoginFormContainer/LoginFormContainer';
 import OLAFLayout from '../../../layouts/OLAFLayout/OLAFLayout';
 import withApollo from '../../../hocs/withApollo';
 import { getUrlParam, OLAFQueryParams } from '../../../utils/url';
+import {
+  PersonByToken,
+  PersonByTokenVariables,
+} from '../../../../generated/PersonByToken';
+
+const PERSON_BY_TOKEN_QUERY = gql`
+  query PersonByToken($token: String!) {
+    personByToken(token: $token) {
+      uuid
+    }
+  }
+`;
+
+const handleAccountFetchError = () =>
+  toast.error(
+    'Sorry there seems to be an issue with your request. Pleaser try again in a few moments',
+    'Dolor ut tempor eiusmod enim consequat laboris dolore ut pariatur labore sunt incididunt dolore veniam mollit excepteur dolor aliqua minim nostrud adipisicing culpa aliquip ex',
+  );
 
 type QueryParams = OLAFQueryParams & {
   uuid: string;
@@ -19,6 +39,23 @@ const AboutYouPage: NextPage = () => {
   const [isLogInVisible, toggleLogInVisibility] = useState(false);
   const router = useRouter();
   const { derivativeId, orderId, uuid } = router.query as QueryParams;
+  const [getPersonByToken] = useLazyQuery<
+    PersonByToken,
+    PersonByTokenVariables
+  >(PERSON_BY_TOKEN_QUERY, {
+    onCompleted: data => {
+      if (data?.personByToken?.uuid) {
+        const currentUrl = '/olaf/about/[uuid]';
+        const redirectUrl = currentUrl.replace(
+          '[uuid]',
+          data.personByToken.uuid,
+        );
+        // reddirect on the same page, with users uuid
+        router.push(currentUrl, redirectUrl, { shallow: true });
+      }
+    },
+    onError: handleAccountFetchError,
+  });
 
   return (
     <OLAFLayout>
@@ -40,7 +77,21 @@ const AboutYouPage: NextPage = () => {
               />
             </div>
           </div>
-          {isLogInVisible && <LoginFormContainer />}
+          {isLogInVisible && (
+            <LoginFormContainer
+              onCompleted={data => {
+                console.log({ dataLogin: data });
+                // request person account after login
+                if (data.login !== null) {
+                  getPersonByToken({
+                    variables: {
+                      token: data.login,
+                    },
+                  });
+                }
+              }}
+            />
+          )}
         </>
       )}
       <AboutFormContainer
