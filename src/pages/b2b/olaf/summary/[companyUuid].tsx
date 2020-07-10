aee6843ddb70885415672638aa1f9ef7c24f7bba
Anastasiya Garbuz <36642340+StationToStation@users.noreply.github.com>
2020-07-10 15:34:45 +0300

    Create [companyUuid].tsx

---
+++ src/pages/b2b/olaf/summary/[companyUuid].tsx
@@ -0,0 +1,22 @@
+import { getDataFromTree } from '@apollo/react-ssr';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import OLAFLayout from '../../../../layouts/OLAFLayout/OLAFLayout';
+import SummaryFormContainer from '../../../../containers/SummaryFormContainer/SummaryFormContainer';
+import withApollo from '../../../../hocs/withApollo';
+
+type QueryParams = {
+  uuid: string;
+};
+
+const SummaryPage: NextPage = () => {
+  const router = useRouter();
+  const { uuid } = router.query as QueryParams;
+  return (
+    <OLAFLayout>
+      <SummaryFormContainer personUuid={uuid} />
+    </OLAFLayout>
+  );
+};
+
+export default withApollo(SummaryPage, { getDataFromTree });
