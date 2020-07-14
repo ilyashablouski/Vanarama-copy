6f4cd0db4753fb17da7d1551cdc72aecf51f71e5
Wafiq Baugh <wafiiiq@gmail.com>
2020-07-14 02:22:25 +0100

    add layout related  helper class

--- src/pages/about-us/customer-testimonials/index.tsx
+++ src/pages/about-us/customer-testimonials/index.tsx
@@ -40,28 +40,33 @@ export const CustomerTestimonialPage: NextPage = () => {
       </div>
       <div className="testimonials--sidebar">...</div>
       <div className="row:bg-lighter">
-        {[1, 2, 3, 4].map(t => (
-          <div key={t}>
-            <Tile className="-plain -button -align-center" plain>
-              <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <Image
-                  inline
-                  round
-                  size="large"
-                  src=" https://source.unsplash.com/collection/2102317/1000x650?sig=403411"
-                />
-              </div>
-              <a className="tile--link" href="##">
-                <Heading tag="span" size="regular" color="black">
-                  Lorem Ipsum Dolor
-                </Heading>
-              </a>
-              <Text tag="p">
-                Pretium facilisi etiam pretium, cras interdum enim, nullam.
-              </Text>
-            </Tile>
-          </div>
-        ))}
+        <div className="row:features-4col">
+          <Heading size="large" color="black">
+            Why Choose Vanarama?
+          </Heading>
+          {[1, 2, 3, 4].map(t => (
+            <div key={t}>
+              <Tile className="-plain -button -align-center" plain>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                  <Image
+                    inline
+                    round
+                    size="large"
+                    src=" https://source.unsplash.com/collection/2102317/1000x650?sig=403411"
+                  />
+                </div>
+                <a className="tile--link" href="##">
+                  <Heading tag="span" size="regular" color="black">
+                    Lorem Ipsum Dolor
+                  </Heading>
+                </a>
+                <Text tag="p">
+                  Pretium facilisi etiam pretium, cras interdum enim, nullam.
+                </Text>
+              </Tile>
+            </div>
+          ))}
+        </div>
       </div>
       <div className="row:text">...</div>
     </div>
