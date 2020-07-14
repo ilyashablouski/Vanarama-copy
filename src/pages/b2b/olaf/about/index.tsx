d49f7fc40d8c3b177cfbff6ef2692c9f1ab1538a
AntonAdamkovich <anton.adamkovich@gmail.com>
2020-07-14 17:31:20 +0300

    Update state of b2b/b2c olaf about pages

--- src/pages/b2b/olaf/about/index.tsx
+++ src/pages/b2b/olaf/about/index.tsx
@@ -26,23 +26,16 @@ const handleAccountFetchError = () =>
     'Dolor ut tempor eiusmod enim consequat laboris dolore ut pariatur labore sunt incididunt dolore veniam mollit excepteur dolor aliqua minim nostrud adipisicing culpa aliquip ex',
   );
 
-type QueryParams = OLAFQueryParams & {
-  uuid: string;
-};
-
 export const BusinessAboutPage: NextPage = () => {
   const router = useRouter();
   const [isLogInVisible, toggleLogInVisibility] = useState(false);
-  const { derivativeId, orderId, uuid } = router.query as QueryParams;
+  const [personUuid, setPersonUuid] = useState<string | null>(null);
+  const { derivativeId, orderId } = router.query as OLAFQueryParams;
 
-  const [getPersonByToken] = usePersonByTokenLazyQuery(data => {
-    if (data?.personByToken?.uuid) {
-      const currentUrl = '/b2b/olaf/about/[uuid]';
-      const redirectUrl = currentUrl.replace('[uuid]', data.personByToken.uuid);
-      // reddirect on the same page, with users uuid
-      router.push(currentUrl, redirectUrl, { shallow: true });
-    }
-  }, handleAccountFetchError);
+  const [getPersonByToken] = usePersonByTokenLazyQuery(
+    data => setPersonUuid(data?.personByToken?.uuid),
+    handleAccountFetchError,
+  );
 
   return (
     <OLAFLayout>
@@ -53,7 +46,7 @@ export const BusinessAboutPage: NextPage = () => {
         To get you your brand new vehicle, firstly we’ll just need some details
         about you and your company.
       </Text>
-      {!uuid && (
+      {!personUuid && (
         <div className="-mb-500">
           <div className="-pt-300 -pb-300">
             <Button
@@ -86,7 +79,7 @@ export const BusinessAboutPage: NextPage = () => {
           router.push(url, url.replace('[companyUuid]', companyUuid));
         }}
         onError={handleCreateUpdateBusinessPersonError}
-        personUuid={uuid}
+        personUuid={personUuid}
         onLogInCLick={() => toggleLogInVisibility(true)}
       />
     </OLAFLayout>
