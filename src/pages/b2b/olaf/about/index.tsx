f4da6f80831fc72a07d1a0b7298b7bb7ee474fe3
AntonAdamkovich <anton.adamkovich@gmail.com>
2020-07-16 18:02:00 +0300

    move completion handler out of render

--- src/pages/b2b/olaf/about/index.tsx
+++ src/pages/b2b/olaf/about/index.tsx
@@ -11,6 +11,7 @@ import OLAFLayout from '../../../../layouts/OLAFLayout/OLAFLayout';
 import { getUrlParam, OLAFQueryParams } from '../../../../utils/url';
 import LoginFormContainer from '../../../../containers/LoginFormContainer/LoginFormContainer';
 import BusinessAboutFormContainer from '../../../../containers/BusinessAboutFormContainer';
+import { SubmitResult } from '../../../../containers/BusinessAboutFormContainer/interfaces';
 import { usePersonByTokenLazyQuery } from '../../../olaf/about';
 import { CompanyTypes } from '../../../../models/enum/CompanyTypes';
 
@@ -38,6 +39,20 @@ export const BusinessAboutPage: NextPage = () => {
     data => setPersonUuid(data?.personByToken?.uuid),
     handleAccountFetchError,
   );
+
+  const handleCreateUpdateBusinessPersonCompletion = ({
+    data,
+    companyType,
+  }: SubmitResult) => {
+    if (companyType === CompanyTypes.limited) {
+      const companyUuid = data.createUpdateBusinessPerson!.uuid!;
+      const params = getUrlParam({ derivativeId, orderId });
+      const url = `/b2b/olaf/company-details/[companyUuid]${params}`;
+      router.push(url, url.replace('[companyUuid]', companyUuid));
+    } else {
+      router.push('/b2b/olaf/soletrader/company-details');
+    }
+  };
 
   return (
     <OLAFLayout>
@@ -74,16 +89,7 @@ export const BusinessAboutPage: NextPage = () => {
         </div>
       )}
       <BusinessAboutFormContainer
-        onCompleted={({ data, companyType }) => {
-          if (companyType === CompanyTypes.limited) {
-            const companyUuid = data.createUpdateBusinessPerson!.uuid!;
-            const params = getUrlParam({ derivativeId, orderId });
-            const url = `/b2b/olaf/company-details/[companyUuid]${params}`;
-            router.push(url, url.replace('[companyUuid]', companyUuid));
-          } else {
-            router.push('/b2b/olaf/soletrader/company-details');
-          }
-        }}
+        onCompleted={handleCreateUpdateBusinessPersonCompletion}
         onError={handleCreateUpdateBusinessPersonError}
         personUuid={personUuid}
         onLogInCLick={() => toggleLogInVisibility(true)}
