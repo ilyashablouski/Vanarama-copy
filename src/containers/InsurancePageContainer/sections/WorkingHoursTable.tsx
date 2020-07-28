35c78cd1ac103eb1a6f1f4cc4ad8937577d97e67
Anastasiya Garbuz <36642340+StationToStation@users.noreply.github.com>
2020-07-28 15:35:07 +0300

    add phone to hero card

--- src/containers/InsurancePageContainer/sections/WorkingHoursTable.tsx
+++ src/containers/InsurancePageContainer/sections/WorkingHoursTable.tsx
@@ -6,7 +6,9 @@ import StructuredList from "@vanarama/uibook/lib/components/organisms/structured
 import { IList } from "@vanarama/uibook/lib/components/organisms/structured-list/interfaces";
 import Button from "@vanarama/uibook/lib/components/atoms/button";
 import Link from "@vanarama/uibook/lib/components/atoms/link";
+import Router from 'next/router';
 
+//TODO: add horizontal lines
 const parseTable = (table: any[]): IList[] => table.map((row: any): IList => ({
     label: row.props.children[0].props.children[0].props.value,
     value: row.props.children[1].props.children[0].props.value
@@ -16,16 +18,18 @@ const renderButton = (button: any[]) => (
     <Link
         href={`tel:${button[1].props.value
             .split('')
-            .filter((item: string) => !isNaN(+item))
+            .filter((item: string) => !isNaN(+item) && item !== ' ')
             .join('')
-            .trim()});}`}>
+            .trim()}`}>
         <Button
             label={button[0].props.children[0].props.value}
             color="teal"
             size="large"
             fill="solid"
-            className="-fullwidth" />
-    </Link>);
+            className="-fullwidth"
+        />
+    </Link>
+);
 
 const WorkingHoursTable = ({ body, title }: HeroCard) => (
     <Card className="hero-card">
@@ -35,7 +39,9 @@ const WorkingHoursTable = ({ body, title }: HeroCard) => (
                 source={body || ''}
                 renderers={{
                     paragraph: props => renderButton(props.children),
-                    table: props => <StructuredList {...props} list={parseTable(props.children[1].props.children)} className="-styled-headers -small -opening-times" />
+                    table: props => <StructuredList {...props}
+                        list={parseTable(props.children[1].props.children)}
+                        className="-styled-headers -small -opening-times" />
                 }}
             />
         </div>
