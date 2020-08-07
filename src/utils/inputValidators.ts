226cf2b7ccad7b81645e94b263d38bd57da11124
Timofey <timofey.chukleev@gmail.com>
2020-08-07 14:40:47 +0300

    DIG-3138: update validation for fleet form

--- src/utils/inputValidators.ts
+++ src/utils/inputValidators.ts
@@ -6,22 +6,17 @@ import {
   WORLDWIDE_MOBILE_REGEX,
   NUMBERS_REGEX,
   LETTERS_AND_NUMBERS_REGEX,
+  NAME_REGEX,
 } from './regex';
 
 const MAX_EMAIL_LENGTH = 254;
 
 export const requiredField = (message: string): ValidationOptions => ({
-  required: {
-    value: true,
-    message,
-  },
+  required: { value: true, message },
 });
 
 export const passwordValidator: ValidationOptions = {
-  required: {
-    value: true,
-    message: 'Your Password is required',
-  },
+  required: { value: true, message: 'Your Password is required' },
   pattern: {
     value: PASSWORD_REGEX,
     message: 'Your Password does not meet the requirements',
@@ -29,39 +24,40 @@ export const passwordValidator: ValidationOptions = {
 };
 
 export const newPasswordValidator: ValidationOptions = {
-  required: {
-    value: true,
-    message: 'Please fill in your new password',
-  },
+  required: { value: true, message: 'Please fill in your new password' },
   pattern: {
     value: PASSWORD_REGEX,
     message: 'Your Password does not meet the requirements',
   },
 };
-
 export const confirmPasswordValidator = (
   password: string,
 ): ValidationOptions => ({
   validate: (confirmPassword: string) =>
     password !== confirmPassword ? 'Repeat Password does not match' : undefined,
-  required: {
-    value: true,
-    message: 'Please fill in your repeat password',
-  },
+  required: { value: true, message: 'Please fill in your repeat password' },
 });
 
 export const fullNameValidator = {
-  required: {
-    value: true,
-    message: 'Please enter your full name',
+  required: { value: true, message: 'Please enter your full name' },
+  minLength: {
+    value: 2,
+    message:
+      'Oops, this name’s too short. Please make it 2 characters or more.',
+  },
+  maxLength: {
+    value: 100,
+    message:
+      'Oops, this name’s too long. Please keep it to 100 characters or less.',
+  },
+  pattern: {
+    value: NAME_REGEX,
+    message: 'Please use only letters, apostrophes and dashes.',
   },
 };
 
 export const postcodeValidator = {
-  required: {
-    value: true,
-    message: 'Please enter your postcode',
-  },
+  required: { value: true, message: 'Please enter your postcode' },
   validate: (postcode: string) => {
     if (postcode.replace(' ', '').length < 5) {
       return 'Oops, your postcode looks a little too short';
@@ -78,10 +74,7 @@ export const postcodeValidator = {
 };
 
 export const emailValidator = {
-  required: {
-    value: true,
-    message: 'Please enter your email address',
-  },
+  required: { value: true, message: 'Please enter your email address' },
   maxLength: {
     value: MAX_EMAIL_LENGTH,
     message: `Email address should not exceed ${MAX_EMAIL_LENGTH} characters`,
@@ -92,11 +85,44 @@ export const emailValidator = {
   },
 };
 
-export const phoneNumberValidator = {
-  required: {
-    value: true,
-    message: 'Please enter your mobile number',
+export const firstNameValidator = {
+  required: { value: true, message: 'Please enter your first name' },
+  minLength: {
+    value: 2,
+    message:
+      'Oops, this name’s too short. Please make it 2 characters or more.',
   },
+  maxLength: {
+    value: 50,
+    message:
+      'Oops, this name’s too long. Please keep it to 50 characters or less.',
+  },
+  pattern: {
+    value: NAME_REGEX,
+    message: 'Please use only letters, apostrophes and dashes.',
+  },
+};
+
+export const lastNameValidator = {
+  required: { value: true, message: 'Please enter your last name' },
+  minLength: {
+    value: 2,
+    message:
+      'Oops, this last name’s too short. Please make it 2 characters or more.',
+  },
+  maxLength: {
+    value: 50,
+    message:
+      'Oops, this last name’s too long. Please keep it to 50 characters or less.',
+  },
+  pattern: {
+    value: NAME_REGEX,
+    message: 'Please use only letters, apostrophes and dashes.',
+  },
+};
+
+export const phoneNumberValidator = {
+  required: { value: true, message: 'Please enter your mobile number' },
   minLength: {
     value: 11,
     message:
@@ -121,29 +147,20 @@ export const termsAndCons = {
 };
 
 export const annualValidator = (message: string) => ({
-  required: {
-    value: true,
-    message,
-  },
+  required: { value: true, message },
   maxLength: {
     value: 50,
     message:
       'Oops, this seems too long. Please keep it to 50 characters or less.',
   },
-  pattern: {
-    value: NUMBERS_REGEX,
-    message: 'Please only use numbers',
-  },
+  pattern: { value: NUMBERS_REGEX, message: 'Please only use numbers' },
 });
 
 export const requiredTextFieldValidator = (
   message: string,
   maxLength: number,
 ) => ({
-  required: {
-    value: true,
-    message,
-  },
+  required: { value: true, message },
   minLength: {
     value: 2,
     message:
@@ -174,4 +191,22 @@ export const vehicleRegistrationNumberValidator = {
     value: LETTERS_AND_NUMBERS_REGEX,
     message: 'Please only use letters and numbers',
   },
+};
+
+export const companyNameValidator = {
+  required: { value: true, message: 'Please enter your company name' },
+  minLength: {
+    value: 2,
+    message:
+      'Oops, this name’s too short. Please make it 2 characters or more.',
+  },
+  maxLength: {
+    value: 160,
+    message:
+      'Oops, this name’s too long. Please keep it to 160 characters or less.',
+  },
+};
+
+export const fleetSizeValidator = {
+  required: { value: true, message: 'Please enter your fleet size' },
 };
