b9b5666ec63c992ff87d6b1320ea9d08285278c7
Sergey Voytsehovich <s.vaitsakhovich@reply.com>
2020-08-21 17:03:48 +0300

    featured-left featured-right for image

--- src/containers/BreakdownCoverContainer/AdvancedBreakdownCoverContainer.tsx
+++ src/containers/BreakdownCoverContainer/AdvancedBreakdownCoverContainer.tsx
@@ -5,8 +5,13 @@ import Text from '@vanarama/uibook/lib/components/atoms/text';
 import Card from '@vanarama/uibook/lib/components/molecules/cards';
 import Image from '@vanarama/uibook/lib/components/atoms/image';
 import { getFeaturedClassPartial } from '../../utils/layout';
-import { advancedBreakdownCoverPage_advancedBreakdownCoverPage_sections as Section } from '../../../generated/advancedBreakdownCoverPage';
 import RouterLink from '../../components/RouterLink/RouterLink';
+import {
+  advancedBreakdownCoverPage_advancedBreakdownCoverPage_sections as Section,
+  advancedBreakdownCoverPage_advancedBreakdownCoverPage_sections_featured3,
+  advancedBreakdownCoverPage_advancedBreakdownCoverPage_sections_featured1,
+  advancedBreakdownCoverPage_advancedBreakdownCoverPage_sections_featured2,
+} from '../../../generated/advancedBreakdownCoverPage';
 
 interface IProps {
   sections: Section | null;
@@ -24,42 +29,9 @@ const AdvancedBreakdownCoverContainer: FC<IProps> = ({
   const featured2 = sections?.featured2;
   const featured3 = sections?.featured3;
 
-  const featured1Html = (
-    <>
-      {featured1 && (
-        <section className={`row:${getFeaturedClassPartial(featured1)}`}>
-          <div>
-            <Heading
-              color="black"
-              size="lead"
-              tag={featured1.titleTag as keyof JSX.IntrinsicElements}
-            >
-              {featured1.title}
-            </Heading>
-            <Text color="darker" size="regular">
-              <ReactMarkdown
-                source={featured1.body?.replace(/\n/gi, '&nbsp;\n') || ''}
-                renderers={{
-                  link: props => {
-                    const { href, children } = props;
-                    return <RouterLink link={{ href, label: children }} />;
-                  },
-                }}
-              />
-            </Text>
-          </div>
-          <div>
-            {featured1.image?.file?.url && (
-              <Image
-                src={featured1.image?.file?.url}
-                alt={featured1.image?.file?.fileName}
-              />
-            )}
-          </div>
-        </section>
-      )}
-    </>
-  );
+  const featured1Html = getFeaturedHtml(featured1);
+  const featured2Html = getFeaturedHtml(featured2);
+  const featured3Html = getFeaturedHtml(featured3);
 
   const tilesHtml = (
     <>
@@ -95,79 +67,7 @@ const AdvancedBreakdownCoverContainer: FC<IProps> = ({
     </>
   );
 
-  const featured2Html = (
-    <>
-      {featured2 && (
-        <section className={`row:${getFeaturedClassPartial(featured2)}`}>
-          <div>
-            <Heading
-              color="black"
-              size="large"
-              tag={featured2.titleTag as keyof JSX.IntrinsicElements}
-            >
-              {featured2.title}
-            </Heading>
-            <Text color="darker" size="regular">
-              <ReactMarkdown
-                source={featured2.body?.replace(/\n/gi, '&nbsp;\n') || ''}
-                renderers={{
-                  link: props => {
-                    const { href, children } = props;
-                    return <RouterLink link={{ href, label: children }} />;
-                  },
-                }}
-              />
-            </Text>
-          </div>
-          <div>
-            {featured2.image?.file?.url && (
-              <Image
-                src={featured2.image?.file?.url}
-                alt={featured2.image?.file?.fileName}
-              />
-            )}
-          </div>
-        </section>
-      )}
-    </>
-  );
-
-  const featured3Html = (
-    <>
-      {featured3 && (
-        <section className={`row:${getFeaturedClassPartial(featured3)}`}>
-          <div>
-            <Heading
-              color="black"
-              size="large"
-              tag={featured3.titleTag as keyof JSX.IntrinsicElements}
-            >
-              {featured3.title}
-            </Heading>
-            <Text color="darker" size="regular">
-              <ReactMarkdown
-                source={featured3.body?.replace(/\n/gi, '&nbsp;\n') || ''}
-                renderers={{
-                  link: props => {
-                    const { href, children } = props;
-                    return <RouterLink link={{ href, label: children }} />;
-                  },
-                }}
-              />
-            </Text>
-          </div>
-          <div>
-            {featured3.image?.file?.url && (
-              <Image
-                src={featured3.image?.file?.url}
-                alt={featured3.image?.file?.fileName}
-              />
-            )}
-          </div>
-        </section>
-      )}
-    </>
-  );
+ 
 
   return (
     <>
@@ -191,6 +91,66 @@ const AdvancedBreakdownCoverContainer: FC<IProps> = ({
       {featured3Html}
     </>
   );
+};
+
+export function getFeaturedHtml(
+  featured:
+    | advancedBreakdownCoverPage_advancedBreakdownCoverPage_sections_featured1
+    | advancedBreakdownCoverPage_advancedBreakdownCoverPage_sections_featured2
+    | advancedBreakdownCoverPage_advancedBreakdownCoverPage_sections_featured3
+    | null
+    | undefined,
+) {
+  const featuredClass = getFeaturedClassPartial(featured);
+  const imageFirst = featuredClass == 'featured-left';
+  return (
+    <>
+      {featured && (
+        <section className={`row:${featuredClass}`}>
+          {imageFirst && (
+            <div>
+              {featured.image?.file?.url && (
+                <Image
+                  src={featured.image?.file?.url}
+                  alt={featured.image?.file?.fileName}
+                />
+              )}
+            </div>
+          )}
+          <div>
+          <Heading
+              color="black"
+              size="lead"
+              tag={featured.titleTag as keyof JSX.IntrinsicElements}
+            >
+              {featured.title}
+            </Heading>
+            <Text color="darker" size="regular">
+            <ReactMarkdown
+                source={featured.body?.replace(/\n/gi, '&nbsp;\n') || ''}
+                renderers={{
+                  link: props => {
+                    const { href, children } = props;
+                    return <RouterLink link={{ href, label: children }} />;
+                  },
+                }}
+              />
+            </Text>
+          </div>
+          {!imageFirst && (
+            <div>
+               {featured.image?.file?.url && (
+              <Image
+                src={featured.image?.file?.url}
+                alt={featured.image?.file?.fileName}
+              />
+            )}
+            </div>
+          )}
+        </section>
+      )}
+    </>
+  )
 };
 
 export default AdvancedBreakdownCoverContainer;
