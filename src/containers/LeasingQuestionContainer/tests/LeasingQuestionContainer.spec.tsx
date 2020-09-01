2afa811e18bfa88ab7f67d11cf912638ba0348af
Sergey Voytsehovich <s.vaitsakhovich@reply.com>
2020-09-01 09:52:38 +0300

    tests

--- src/containers/LeasingQuestionContainer/tests/LeasingQuestionContainer.spec.tsx
+++ src/containers/LeasingQuestionContainer/tests/LeasingQuestionContainer.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MockedProvider } from '@apollo/client/testing';
 import { screen, render, waitFor } from '@testing-library/react';
-import LeasingQuestionsContainer from '../LeasingQuestionContainer';
+import LeasingQuestionContainer from '../LeasingQuestionContainer';
 
 // ARRANGE
 const CRUMBS = [
@@ -10,33 +10,36 @@ const CRUMBS = [
 ];
 
 const METADATA = {
-  title: 'Customer Leasing Questions',
+  title: 'Insurance, Tax & Vat Leasing Questions',
   metaRobots: null,
-  metaDescription:
-    "At Vanarama, we want to make leasing a brand new car as simple as possible - you'll find all of the information & advice you might need in our collection of easy-to-understand guides to leasing.",
+  metaDescription: 'Insurance Tax Vat',
   publishedOn: null,
-  legacyUrl: 'https://www.vanarama.com/car-leasing-explained.html',
+  legacyUrl:
+    'https://www.vanarama.com/van-leasing-questions/insurance-tax-vat.html',
   pageType: 'Customer Leasing Questions',
   __typename: 'Meta',
 };
-const BODY = `At Vanarama, we can offer you the best car leasing option to suit your needs, whether it's personal contract hire or business contract hire.\n\nWe understand the importance of your new car decision and want to make sure the process is as simple and seamless as possible for you. You can find out how we do this by visiting our page about&nbsp;[How Car Leasing Works](https://beta.vanarama.com/car-leasing-explained.html).\n\nYou can use the links below to view the key features and benefits of the new car finance products available. Our in-depth FAQs will also help with any specific queries. If you need any more information, please feel free to pick up the phone to one of our Account Managers on 01442 835 769 or drop us an email at [enquiries@vanarama.co.uk](mailto:enquiries@vanarama.co.uk).\n\n## Available New Car Finance Leasing Options\n \n- [Business Contract Hire](https://beta.vanarama.com/car-business-contract-hire.html)\n- [Personal Contract Hire](https://beta.vanarama.com/car-personal-contract-hire.html "Personal Contract Hire")\n \n\n\n`;
+const BODY = ``;
 
 const SECTIONS = {
-  carousel: {
-    name: 'Insurance News',
-    cards: [
+  questionSet: {
+    name: 'All Questions',
+    questionAnswers: [
       {
-        titleTag: null,
-        name: 'Do I Need Insurance For A Leased Car?',
-        title: 'Do I Need Insurance For A Leased Car?',
-        body:
-          'Find out what type of coverage you need for a new leased car.\n\n[Read More](https://beta.vanarama.com/car-leasing-explained/do-i-need-insurance-for-leased-car.html)',
-        image: {
-          file: {
-            url:
-              '//images.ctfassets.net/3xid768u5joa/22ybEevo2hRu2pJImPp8tz/327aba1204a84c35b02490ca075c290c/insurance-on-leased-cars.jpg',
-          },
-        },
+        question: "I would like a large van, but still unsure what is the best purchase option? Also what is the best tax relief option through my limited company? I'm not VAT registered, so can I claim back my contract payments?",
+        answer: "Our expert Mike answered... Hello Adrian, thank you very much for leaving your question on our website. There are various leasing options available depending on what you would like to achieve at the end of the contract. If you are not VAT registered then you will not be able to claim the VAT back on the rentals, however depending on which contract you decided to go for, the rentals may well be 100% tax deductible. If you would like to discuss any of these options in greater detail, the best thing to do is contact one of our specialists on 01442 838193 or alternatively please let us know the best contact number for you and when would be a convenient time to contact you. Many thanks once again for leaving your question. Kind regards, Mike Daly.",
+      },
+      {
+        question: "I would like a large van, but still unsure what is the best purchase option? Also what is the best tax relief option through my limited company? I'm not VAT registered, so can I claim back my contract payments?",
+        answer: "Our expert Mike answered... Hello Adrian, thank you very much for leaving your question on our website. There are various leasing options available depending on what you would like to achieve at the end of the contract. If you are not VAT registered then you will not be able to claim the VAT back on the rentals, however depending on which contract you decided to go for, the rentals may well be 100% tax deductible. If you would like to discuss any of these options in greater detail, the best thing to do is contact one of our specialists on 01442 838193 or alternatively please let us know the best contact number for you and when would be a convenient time to contact you. Many thanks once again for leaving your question. Kind regards, Mike Daly.",
+      },
+      {
+        question: "I would like a large van, but still unsure what is the best purchase option? Also what is the best tax relief option through my limited company? I'm not VAT registered, so can I claim back my contract payments?",
+        answer: "Our expert Mike answered... Hello Adrian, thank you very much for leaving your question on our website. There are various leasing options available depending on what you would like to achieve at the end of the contract. If you are not VAT registered then you will not be able to claim the VAT back on the rentals, however depending on which contract you decided to go for, the rentals may well be 100% tax deductible. If you would like to discuss any of these options in greater detail, the best thing to do is contact one of our specialists on 01442 838193 or alternatively please let us know the best contact number for you and when would be a convenient time to contact you. Many thanks once again for leaving your question. Kind regards, Mike Daly.",
+      },
+        {
+        question: "I would like a large van, but still unsure what is the best purchase option? Also what is the best tax relief option through my limited company? I'm not VAT registered, so can I claim back my contract payments?",
+        answer: "Our expert Mike answered... Hello Adrian, thank you very much for leaving your question on our website. There are various leasing options available depending on what you would like to achieve at the end of the contract. If you are not VAT registered then you will not be able to claim the VAT back on the rentals, however depending on which contract you decided to go for, the rentals may well be 100% tax deductible. If you would like to discuss any of these options in greater detail, the best thing to do is contact one of our specialists on 01442 838193 or alternatively please let us know the best contact number for you and when would be a convenient time to contact you. Many thanks once again for leaving your question. Kind regards, Mike Daly.",
       },
     ],
   },
@@ -47,59 +50,40 @@ const SECTIONS = {
     description: null,
     cards: [
       {
-        title: 'Company Cash Allowance Vs Company Car',
-        name: 'Company Cash Allowance Vs Company Car',
-        image: {
-          title: 'company-cash-allowance-vs-company-car',
-          description: null,
-          file: {
-            url:
-              '//images.ctfassets.net/3xid768u5joa/FRg0wIDLXZqVIB9ZoJEw7/e04f3cfdfb2ad9405be84ef106d83d26/company-cash-allowance-vs-company-car.jpg',
-            fileName: 'company-cash-allowance-vs-company-car.jpg',
-            contentType: 'image/jpeg',
-          },
-        },
+        titleTag: 'h3',
+        name: 'Question 1',
+        title: 'Is Road Tax Covered In The Lease?',
         body:
-          'So what should you consider if you are a company car driver reviewing your options?\n\n[Read More](https://beta.vanarama.com/car-leasing-explained/given-company-cash-allowance-vs-company-car.html)',
-        titleTag: null,
-        link: {
-          text: 'Read More',
-          url:
-            'https://betavanarama.com/van-leasing-explained/how-does-van-leasing-work.html',
-        },
+          "It depends on which lease product you choose. If you choose a contract hire or contract purchase the road tax is included for the duration of the contract. Finance lease will include the first year's road tax, however, the finance company will then invoice you for the next year.",
+        image: null,
       },
       {
-        title: 'What Happens At The End Of A Car Lease?',
-        name: 'What Happens At The End Of A Car Lease?',
-        image: {
-          title: 'what-happens-at-end-of-car-lease',
-          description: null,
-          file: {
-            url:
-              '//images.ctfassets.net/3xid768u5joa/2gBj5lixlkrla8kFNF8XSi/0fea9544b48e892f4f96d612b9be9c70/what-happens-at-end-of-car-lease.jpg',
-            fileName: 'what-happens-at-end-of-car-lease.jpg',
-            contentType: 'image/jpeg',
-          },
-        },
+        titleTag: 'h3',
+        name: 'Question 1',
+        title: 'Is Road Tax Covered In The Lease?2',
         body:
-          'At the end of a lease contract, you simply hand back the car to the finance company...\n\n[Read More](https://beta.vanarama.com/car-leasing-explained/what-happens-at-end-of-car-lease.html)',
-        titleTag: null,
-        link: {
-          text: 'Read More',
-          url:
-            'https://betavanarama.com/van-leasing-explained/how-does-van-leasing-work.html',
-        },
+          "It depends on which lease product you choose. If you choose a contract hire or contract purchase the road tax is included for the duration of the contract. Finance lease will include the first year's road tax, however, the finance company will then invoice you for the next year.",
+        image: null,
       },
+      {
+        titleTag: 'h3',
+        name: 'Question 1',
+        title: 'Is Road Tax Covered In The Lease?3',
+        body:
+          "It depends on which lease product you choose. If you choose a contract hire or contract purchase the road tax is included for the duration of the contract. Finance lease will include the first year's road tax, however, the finance company will then invoice you for the next year.",
+        image: null,
+      },
+      ,
     ],
   },
 } as any;
 
-describe('<FinanceExplainedContainer />', () => {
+describe('<LeasingQuestionContainer />', () => {
   it('should match snapshot', async () => {
     // ACT
     const getComponent = render(
       <MockedProvider addTypename={false}>
-        <LeasingQuestionsContainer
+        <LeasingQuestionContainer
           crumbs={CRUMBS}
           sections={SECTIONS}
           title={METADATA.title}
@@ -109,7 +93,7 @@ describe('<FinanceExplainedContainer />', () => {
     );
     // ASSERT
     await waitFor(() => {
-      expect(screen.getByText(`Most Popular Questions`)).toBeInTheDocument();
+      expect(screen.getByText(`Is Road Tax Covered In The Lease?`)).toBeInTheDocument();
     });
 
     const tree = getComponent.baseElement;
