9c3a6c6a837fd8282300ab814239f50ad09564de
Timofey Chukleev <timofey.chukleev@gmail.com>
2020-09-04 14:48:28 +0300

    update unit test

--- src/__tests__/pages/hub/hubVanPage.spec.tsx
+++ src/__tests__/pages/hub/hubVanPage.spec.tsx
@@ -23,7 +23,17 @@ jest.mock('../../../containers/SearchPodContainer', () => () => {
 });
 jest.mock('../../../containers/OrdersInformation/gql');
 
-jest.mock('next/router', () => ({ push: jest.fn() }));
+jest.mock('next/router', () => ({
+  push: jest.fn(),
+  useRouter() {
+    return {
+      asPath: '/hub/vans',
+      query: {
+        score: 75,
+      },
+    };
+  },
+}));
 
 const mocked: MockedResponse[] = [
   {
@@ -520,9 +530,7 @@ describe('<VansPage />', () => {
 
   it('should successfully query all hub VansPage data', async () => {
     await waitFor(() => {
-      expect(
-        screen.getByText('Why Choose Vanarama For Your Van?'),
-      ).toBeInTheDocument();
+      expect(screen.getByText('DEAL OF THE MONTH')).toBeInTheDocument();
     });
   });
 
