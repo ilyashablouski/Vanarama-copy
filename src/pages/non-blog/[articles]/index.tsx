e00055978b64cd5fc96eec4b7bcdbdda3d554b00
Kira Emchik <kira.emchik@gmail.com>
2020-09-16 15:48:21 +0300

    Added ErrorMessage

--- src/pages/non-blog/[articles]/index.tsx
+++ src/pages/non-blog/[articles]/index.tsx
@@ -5,6 +5,7 @@ import { useGenericPage } from '../../../gql/genericPage';
 import Head from '../../../components/Head/Head';
 import withApollo from '../../../hocs/withApollo';
 import BlogPostContainer from '../../../containers/BlogPostContainer/BlogPostContainer';
+import ErrorMessage from '../../../components/ErrorMessage/ErrorMessage';
 
 const NonBlogPost: NextPage = () => {
   const router = useRouter();
@@ -16,7 +17,7 @@ const NonBlogPost: NextPage = () => {
   }
 
   if (error) {
-    return <p>Error: {error.message}</p>;
+    return <ErrorMessage message={error.message} />;
   }
 
   const body = data?.genericPage?.body;
