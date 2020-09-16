356f828da81efa76227ecdf53be194dabf729ba5
AntonAdamkovich <anton.adamkovich@gmail.com>
2020-09-16 14:58:33 +0300

    Update tests structure

--- src/utils/authentication.spec.ts
+++ src/utils/authentication.spec.ts
@@ -1,33 +1,37 @@
 import { isUserAuthenticated } from './authentication';
 
-describe('isUserAuthenticated', () => {
-  const ID_COOKIE_NAME = 'ic';
-  const ID_COOKIE_VALUE = 'TEST_COOKIE_VALUE';
+const ID_COOKIE_NAME = 'ic';
+const ID_COOKIE_VALUE = 'TEST_COOKIE_VALUE';
 
-  it('should return that user is NOT authenticated because of empty cookies', () => {
+describe('WHEN ID cookie is empty', () => {
+  it('should return false', () => {
     const cookie = '';
     document.cookie = cookie;
 
-    const isuthenticated = isUserAuthenticated();
-    expect(isuthenticated).toBe(false);
+    const isUthenticated = isUserAuthenticated();
+    expect(isUthenticated).toBe(false);
   });
+});
 
-  it('should return that suer is NOT uthenticated because cookie is expired', () => {
+describe('WHEN ID cookie is expired', () => {
+  it('should return false', () => {
     const expiredDate = new Date(2000, 1, 1);
     const cookie = `${ID_COOKIE_NAME}=${ID_COOKIE_VALUE}; Expires=${expiredDate}`;
     document.cookie = cookie;
 
-    const isuthenticated = isUserAuthenticated();
-    expect(isuthenticated).toBe(false);
+    const isUthenticated = isUserAuthenticated();
+    expect(isUthenticated).toBe(false);
   });
+});
 
-  it('should return that user is authenticated', () => {
+describe('WHEN ID cookie is NOT empty AND is NOT expired', () => {
+  it('should return true', () => {
     const validDate = new Date();
     validDate.setFullYear(validDate.getFullYear() + 1);
     const cookie = `${ID_COOKIE_NAME}=${ID_COOKIE_VALUE}; Expires=${validDate}`;
     document.cookie = cookie;
 
-    const isuthenticated = isUserAuthenticated();
-    expect(isuthenticated).toBe(true);
+    const isUthenticated = isUserAuthenticated();
+    expect(isUthenticated).toBe(true);
   });
 });
