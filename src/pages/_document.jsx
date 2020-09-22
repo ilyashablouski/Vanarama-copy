65ab7f75a2f938e5dca40e59a6e2f7eea0daf79d
Kira Emchik <kira.emchik@gmail.com>
2020-09-22 13:20:46 +0300

    DIG 3633

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -11,7 +11,7 @@ import {
 // const { publicRuntimeConfig } = getConfig();
 
 // GTM.
-const gtmEnvs = ['test', 'production', 'development'];
+const gtmEnvs = ['test', 'dev', 'production', 'development'];
 
 // Rollbar
 // const rollbarCode = `
