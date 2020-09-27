8073a19ec0dba1cb30b58c6c7142e91b13e03cca
Kira Emchik <kira.emchik@gmail.com>
2020-09-27 22:24:24 +0300

    Added test

--- src/containers/CategoryPageContainer/interface.tsx
+++ src/containers/CategoryPageContainer/interface.tsx
@@ -1,16 +1,12 @@
 /* eslint-disable @typescript-eslint/camelcase */
 import {
-  GenericPageQuery_genericPage_sections_tiles,
   GenericPageQuery_genericPage_sections_carousel,
-  GenericPageQuery_genericPage_sections_featured,
   GenericPageQuery_genericPage_metaData,
   GenericPageQuery_genericPage_featuredImage,
 } from '../../../generated/GenericPageQuery';
 
 export interface ICategoryPage {
-  featured: GenericPageQuery_genericPage_sections_featured | null | undefined;
   carousel: GenericPageQuery_genericPage_sections_carousel | null | undefined;
-  tiles: GenericPageQuery_genericPage_sections_tiles | null | undefined;
   metaData: GenericPageQuery_genericPage_metaData | undefined;
-  featuredImage: GenericPageQuery_genericPage_featuredImage | null | undefined;
+  featuredImage?: GenericPageQuery_genericPage_featuredImage | null | undefined;
 }
