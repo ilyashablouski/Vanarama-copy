9e7dbe1d83c163b8fffb90c3d0e4f2797575d9ce
Wafiq Baugh <wafiiiq@gmail.com>
2020-09-30 17:17:08 +0100

    fix test soleTraderCompanyDetailsForm

--- src/components/SoleTraderCompanyDetailsForm/SoleTraderCompanyDetailsForm.tsx
+++ src/components/SoleTraderCompanyDetailsForm/SoleTraderCompanyDetailsForm.tsx
@@ -232,7 +232,7 @@ const SoleTraderCompanyDetailsForm: React.FC<ISoleTraderCompanyDetailsFormProps>
           name="existingVehicle"
           label="Existing Vehicle Finance to be Replaced?"
           ref={register}
-          defaultChecked={defaultValues.monthlyAmountBeingReplaced !== ''}
+          defaultChecked={defaultValues?.monthlyAmountBeingReplaced !== ''}
         />
       </Formgroup>
       {existingVehicle && (
