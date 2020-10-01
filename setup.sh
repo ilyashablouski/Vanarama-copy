5c4ab5d888120f49f4ac3ff6ba8b335da0ab3cac
roddyherries <roddy@techamigos.co>
2020-10-01 15:08:36 +0100

    Tweak GTM_ID param support

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ STACK=${2}
 APP=${3}
 REGION=${4}
 CURRENTBRANCH=${5}
-GTM_ID=${6}
 
 export API_KEY="$(aws ssm get-parameter --name "/$ENV/$STACK/$APP/fed-gateway-api-key" --region $REGION --with-decryption | jq -r ".Parameter.Value")"
 export API_URL="$(aws ssm get-parameter --name "/$ENV/$STACK/$APP/gateway-api-url" --region $REGION --with-decryption | jq -r ".Parameter.Value")"
@@ -12,4 +11,4 @@ export LOQATE_KEY="$(aws ssm get-parameter --name "/$ENV/$STACK/$APP/loqate-key"
 export GITHUB_TOKEN="$(aws ssm get-parameter --name "/$ENV/$STACK/$APP/github-token" --region $REGION --with-decryption | jq -r ".Parameter.Value")"
 export ROLLBAR_CLIENT_TOKEN="$(aws ssm get-parameter --name "/$ENV/$STACK/$APP/rollbar-client-token" --region $REGION --with-decryption | jq -r ".Parameter.Value")"
 export ROLLBAR_SERVER_TOKEN="$(aws ssm get-parameter --name "/$ENV/$STACK/$APP/rollbar-server-token" --region $REGION --with-decryption | jq -r ".Parameter.Value")"
-export GTM_ID="$(GTM_ID)"
+export GTM_ID="$(aws ssm get-parameter --name "/$ENV/$STACK/$APP/gtm-id" --region $REGION --with-decryption | jq -r ".Parameter.Value")"
