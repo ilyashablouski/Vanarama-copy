7569f9e417a9f318a623c17e81d1cc85907717c0
AntonAdamkovich <anton.adamkovich@gmail.com>
2020-10-09 16:04:18 +0300

    Update generated types

---
+++ generated/SortOrderQuery.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: SortOrderQuery
+// ====================================================
+
+export interface SortOrderQuery {
+  sortOrder: any | null;
+}
