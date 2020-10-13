235d3a808b377308f5ca437c3ea985df7b0a6c76
Kira Emchik <kira.emchik@gmail.com>
2020-10-13 19:08:03 +0300

    DIG-3645

---
+++ src/utils/articles.ts
@@ -0,0 +1,30 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable @typescript-eslint/camelcase */
+import moment from 'moment';
+import { NextRouter } from 'next/router';
+import { BlogPosts_blogPosts_articles } from '../../generated/BlogPosts';
+
+export const getArticles = (
+  articles: (BlogPosts_blogPosts_articles | null)[] | null | undefined,
+  router: NextRouter,
+) => {
+  const articlesSorted = articles
+    ? [...articles]?.sort((firstArticle, secondArticle) =>
+        moment(secondArticle?.publishedOn).diff(
+          moment(firstArticle?.publishedOn),
+        ),
+      )
+    : [];
+
+  let firstArticles = articlesSorted.slice(0, 3);
+  const isCurrentBlog = firstArticles.find(
+    article => article?.slug === router.asPath.slice(1),
+  );
+
+  if (isCurrentBlog) {
+    const index = firstArticles.indexOf(isCurrentBlog);
+    firstArticles.splice(index, 1);
+    firstArticles = [...firstArticles, ...articlesSorted.slice(3, 4)];
+  }
+  return firstArticles;
+};
