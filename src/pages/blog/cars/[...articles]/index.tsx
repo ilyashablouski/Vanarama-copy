a0c92be253afca66ec11c65263aafe439f5806ae
Kira Emchik <kira.emchik@gmail.com>
2020-10-13 18:41:11 +0300

    DIG-3845

--- src/pages/blog/cars/[...articles]/index.tsx
+++ src/pages/blog/cars/[...articles]/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import { NextPage } from 'next';
 import Loading from '@vanarama/uibook/lib/components/atoms/loading';
 import { useRouter } from 'next/router';
@@ -6,19 +7,30 @@ import { useBlogPostPage } from '../../../../gql/blogPost';
 import BlogPostContainer from '../../../../containers/BlogPostContainer/BlogPostContainer';
 import ErrorMessage from '../../../../components/ErrorMessage/ErrorMessage';
 import { getSectionsData } from '../../../../utils/getSectionsData';
+import { useBlogPostsPage } from '../../../../gql/blogPosts';
+import { getArticles } from '../../../../utils/articles';
 
 const BlogPost: NextPage = () => {
   const router = useRouter();
   const { data, loading, error } = useBlogPostPage(router.asPath.slice(1));
 
-  if (loading) {
+  const {
+    data: blogPosts,
+    loading: blogPostsLoading,
+    error: blogPostsError,
+  } = useBlogPostsPage('blog/cars');
+
+  if (loading || blogPostsLoading) {
     return <Loading size="large" />;
   }
 
-  if (error) {
-    return <ErrorMessage message={error.message} />;
+  if (error || blogPostsError) {
+    return (
+      <ErrorMessage message={error?.message || blogPostsError?.message || ''} />
+    );
   }
 
+  const articles = getSectionsData(['blogPosts', 'articles'], blogPosts);
   const body = getSectionsData(['body'], data?.blogPost);
   const name = getSectionsData(['metaData', 'name'], data?.blogPost);
   const image = getSectionsData(
@@ -29,6 +41,8 @@ const BlogPost: NextPage = () => {
   const breadcrumbsItems = metaData?.breadcrumbs?.map((el: any) => ({
     link: { href: el.href || '', label: el.label },
   }));
+  console.log('articles', getArticles(articles, router));
+  console.log('data', data);
 
   return (
     <BlogPostContainer
