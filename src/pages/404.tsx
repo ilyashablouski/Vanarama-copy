08737b9d64a96612655b238b877daad313a3c768
Aliaksei Douhal <kcafmil@gmail.com>
2020-10-20 13:09:55 +0300

    lint

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const PageNotFound = () => {
-    return (
-        <div>
-            404
-        </div>
-    );
+  return <div>404</div>;
 };
 
 export default PageNotFound;
