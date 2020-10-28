58a21198bc8641d6680b5a4ceb2019a6c0e87999
Kira Emchik <kira.emchik@gmail.com>
2020-10-28 12:20:59 +0300

    DIG-3896

--- src/utils/blogSlugs.ts
+++ src/utils/blogSlugs.ts
@@ -6,7 +6,13 @@ export const getPaths = (blogPosts: BlogPosts_blogPosts | undefined | null) => {
   const slugs = blogPosts?.articles?.map(article =>
     article?.slug?.split('/').pop(),
   );
-  return slugs?.map(slug => ({
-    params: { articles: [slug] },
-  }));
+  const paths = slugs?.map(slug => {
+    if (slug) {
+      return {
+        params: { articles: [slug] },
+      };
+    }
+    return false;
+  });
+  return paths?.filter(path => path);
 };
