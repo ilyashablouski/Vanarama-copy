e44fd5e719ae15998e0ce3ad794f62d3d14538e1
Wafiq Baugh <wafiiiq@gmail.com>
2020-11-02 11:41:57 +0000

    update package.json

--- src/utils/layout.ts
+++ src/utils/layout.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export function getFeaturedClassPartial(featured: any) {
   if (featured?.layout?.includes('Full Width')) {
-    // return 'featured-fullwidth';
+    return 'featured-fullwidth';
   }
 
   return featured?.layout?.includes('Media Left')
