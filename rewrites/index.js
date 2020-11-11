98f7193a8e195228914ac09d461d43bc3a5eb3e6
AntonAdamkovich <anton.adamkovich@gmail.com>
2020-11-11 15:49:38 +0300

    Return previous rewrite url

--- rewrites/index.js
+++ rewrites/index.js
@@ -5,7 +5,7 @@ async function fetchRewritesList() {
 
   // Get list from dev S3 bucket.
   const raw = await fetch(
-    'https://s3.eu-west-2.amazonaws.com/static.vanarama-nonprod.com/rewrites/dev/rewrites.json',
+    'https://devstatic.vanarama-nonprod.com/rewrites/dev/rewrites.json',,
   );
   const json = await raw.json();
 
