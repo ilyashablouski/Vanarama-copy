2da8eb5ad55b8a210c44ede1717c38a68d5abfec
AntonAdamkovich <anton.adamkovich@gmail.com>
2020-11-13 16:22:55 +0300

    Return back error message

--- src/pages/van-leasing/[...details-page]/index.tsx
+++ src/pages/van-leasing/[...details-page]/index.tsx
@@ -148,7 +148,7 @@ export async function getServerSideProps(context: NextPageContext) {
 
     return {
       props: {
-        errors: error.graphQLErrors,
+        errors: error.message,
       },
     };
   }
