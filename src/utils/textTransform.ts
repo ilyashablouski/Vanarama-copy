9478d3045f6b49b6ccc388241678dcca0023fb37
Kira Emchik <kira.emchik@gmail.com>
2020-11-13 11:50:00 +0300

    DIG-4218

---
+++ src/utils/textTransform.ts
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+
+export const capitalizeFirstLetter = (text: string | undefined) => {
+  if (!text) return '';
+  return `${text.charAt(0).toUpperCase() + text.slice(1)}`;
+};
