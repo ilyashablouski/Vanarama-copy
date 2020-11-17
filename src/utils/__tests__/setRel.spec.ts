0feebf8430b30213196c6140b83297c2175373c4
iamgnlc <gianluca.agnocchetti@autorama.co.uk>
2020-11-17 13:12:00 +0000

    moved env var declaration into it block

--- src/utils/__tests__/setRel.spec.ts
+++ src/utils/__tests__/setRel.spec.ts
@@ -1,18 +1,15 @@
 import setRel from '../setRel';
 
 describe('setRel', () => {
-  beforeEach(() => {
-    process.env.HOSTNAME = 'https://vanarama.com';
-  });
   it('should return "noopener noreferrer" for external links', () => {
+    process.env.HOSTNAME = 'https://vanarama.com';
     const link = { label: 'Google', href: 'https://www.google.com' };
     const actual = setRel(link);
-
-    console.log(process.env.HOSTNAME);
 
     expect(actual).toEqual('noopener noreferrer');
   });
   it('should return undefined for internal links', () => {
+    process.env.HOSTNAME = 'https://vanarama.com';
     const link = {
       label: 'About us',
       href: 'https://www.vanarama.com/about-us',
