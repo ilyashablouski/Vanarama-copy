289e8792fbc50685e889f04e26574b104c91827e
iamgnlc <gianluca.agnocchetti@autorama.co.uk>
2020-11-17 13:30:45 +0000

    fix var

--- src/utils/setRel.ts
+++ src/utils/setRel.ts
@@ -1,6 +1,8 @@
 import { ILink } from '@vanarama/uibook/lib/interfaces/link';
 
-const setRel = (link: ILink, hostname = process?.env?.HOSTNAME) => {
+const setRel = (link: ILink, hostname: string | undefined) => {
+  // eslint-disable-next-line no-param-reassign
+  if (!hostname) hostname = process?.env?.HOSTNAME;
   const domain = hostname?.split('//');
 
   return domain?.length &&
