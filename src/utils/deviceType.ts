94e0fdce11f33494fd27dad46b0cb97d04672c6f
Kira Emchik <kira.emchik@gmail.com>
2020-11-19 12:30:22 +0300

    Fix

--- src/utils/deviceType.ts
+++ src/utils/deviceType.ts
@@ -1,7 +1,7 @@
 import { isMobileOnly, isTablet } from 'react-device-detect';
 
 export const DEVICE_TYPES = {
-  desctop: 'Desctop',
+  desktop: 'Desktop',
   mobile: 'Mobile',
   tablet: 'Tablet',
 };
@@ -13,5 +13,5 @@ export const getDeviceType = () => {
   if (isMobileOnly) {
     return DEVICE_TYPES.mobile;
   }
-  return DEVICE_TYPES.desctop;
+  return DEVICE_TYPES.desktop;
 };
