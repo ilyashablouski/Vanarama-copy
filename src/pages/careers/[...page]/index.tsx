72738562a4f0b47cbbc07a5a8909664738dde71b
Timofey Chukleev <timofey.chukleev@gmail.com>
2020-11-22 16:13:46 +0300

    DIG-4330: add Generate static pages for Careers

--- src/pages/careers/[...page]/index.tsx
+++ src/pages/careers/[...page]/index.tsx
@@ -1,14 +1,82 @@
-import { useRouter } from 'next/router';
-import { NextPage } from 'next';
-import { useGenericPage } from '../../../gql/genericPage';
-import withApollo from '../../../hocs/withApollo';
+import dynamic from 'next/dynamic';
+import DefaultErrorPage from 'next/error';
+import { GetStaticPropsContext, NextPage, NextPageContext } from 'next';
+import { GENERIC_PAGE, IGenericPage } from '../../../gql/genericPage';
 import SimplePageContainer from '../../../containers/SipmlePageContainer/SipmlePageContainer';
+import createApolloClient from '../../../apolloClient';
+import { PAGE_COLLECTION } from '../../../gql/pageCollection';
+import { getPathsFromPageCollection } from '../../../utils/pageSlugs';
+import {
+  PageCollection,
+  PageCollectionVariables,
+} from '../../../../generated/PageCollection';
+import { getSectionsData } from '../../../utils/getSectionsData';
 
-const CareerPage: NextPage = () => {
-  const router = useRouter();
-  const { data, loading, error } = useGenericPage(router.asPath.slice(1));
+const Breadcrumb = dynamic(() =>
+  import('../../../components/Breadcrumb/Breadcrumb'),
+);
 
-  return <SimplePageContainer data={data} loading={loading} error={error} />;
+const CareerPage: NextPage<IGenericPage> = ({ data, error, loading }) => {
+  if (error || !data?.genericPage) {
+    return <DefaultErrorPage statusCode={404} />;
+  }
+
+  const metaData = getSectionsData(['metaData'], data?.genericPage);
+
+  const breadcrumbsItems = metaData?.breadcrumbs?.map((el: any) => ({
+    link: { href: el.href || '', label: el.label },
+  }));
+
+  return (
+    <>
+      <div className="row:title">
+        <Breadcrumb items={breadcrumbsItems} />
+      </div>
+      <SimplePageContainer data={data} loading={!!loading} error={error} />
+    </>
+  );
 };
 
-export default withApollo(CareerPage);
+export async function getStaticPaths() {
+  const client = createApolloClient({});
+  const { data } = await client.query<PageCollection, PageCollectionVariables>({
+    query: PAGE_COLLECTION,
+    variables: {
+      pageType: 'Careers',
+    },
+  });
+  const items = data?.pageCollection?.items;
+
+  return {
+    paths: getPathsFromPageCollection(items, 'careers'),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  try {
+    const client = createApolloClient({}, context as NextPageContext);
+    const paths = context?.params?.page as string[];
+
+    const { data, errors } = await client.query({
+      query: GENERIC_PAGE,
+      variables: {
+        slug: `careers/${paths?.join('/')}`,
+      },
+    });
+    return {
+      props: {
+        data,
+        error: errors ? errors[0] : null,
+      },
+    };
+  } catch {
+    return {
+      props: {
+        error: true,
+      },
+    };
+  }
+}
+
+export default CareerPage;
