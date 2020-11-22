d1f4cb357839717510c196849f4a218cdf0ec20c
Timofey Chukleev <timofey.chukleev@gmail.com>
2020-11-22 16:13:34 +0300

    DIG-4330: add Generate static pages for Authors

---
+++ src/pages/authors/[...author]/index.tsx
@@ -0,0 +1,82 @@
+import dynamic from 'next/dynamic';
+import DefaultErrorPage from 'next/error';
+import { GetStaticPropsContext, NextPage, NextPageContext } from 'next';
+import { GENERIC_PAGE, IGenericPage } from '../../../gql/genericPage';
+import SimplePageContainer from '../../../containers/SipmlePageContainer/SipmlePageContainer';
+import createApolloClient from '../../../apolloClient';
+import { PAGE_COLLECTION } from '../../../gql/pageCollection';
+import { getPathsFromPageCollection } from '../../../utils/pageSlugs';
+import {
+  PageCollection,
+  PageCollectionVariables,
+} from '../../../../generated/PageCollection';
+import { getSectionsData } from '../../../utils/getSectionsData';
+
+const Breadcrumb = dynamic(() =>
+  import('../../../components/Breadcrumb/Breadcrumb'),
+);
+
+const AuthorPage: NextPage<IGenericPage> = ({ data, error, loading }) => {
+  if (error || !data?.genericPage) {
+    return <DefaultErrorPage statusCode={404} />;
+  }
+
+  const metaData = getSectionsData(['metaData'], data?.genericPage);
+
+  const breadcrumbsItems = metaData?.breadcrumbs?.map((el: any) => ({
+    link: { href: el.href || '', label: el.label },
+  }));
+
+  return (
+    <>
+      <div className="row:title">
+        <Breadcrumb items={breadcrumbsItems} />
+      </div>
+      <SimplePageContainer data={data} loading={!!loading} error={error} />
+    </>
+  );
+};
+
+export async function getStaticPaths() {
+  const client = createApolloClient({});
+  const { data } = await client.query<PageCollection, PageCollectionVariables>({
+    query: PAGE_COLLECTION,
+    variables: {
+      pageType: 'Authors',
+    },
+  });
+  const items = data?.pageCollection?.items;
+
+  return {
+    paths: getPathsFromPageCollection(items, 'authors'),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  try {
+    const client = createApolloClient({}, context as NextPageContext);
+    const paths = context?.params?.author as string[];
+
+    const { data, errors } = await client.query({
+      query: GENERIC_PAGE,
+      variables: {
+        slug: `authors/${paths?.join('/')}`,
+      },
+    });
+    return {
+      props: {
+        data,
+        error: errors ? errors[0] : null,
+      },
+    };
+  } catch {
+    return {
+      props: {
+        error: true,
+      },
+    };
+  }
+}
+
+export default AuthorPage;
