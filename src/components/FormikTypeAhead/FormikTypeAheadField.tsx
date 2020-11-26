ae4b87c6a268e72e00a85d476413ffda4e1f79d4
Wafiq Baugh <wafiiiq@gmail.com>
2020-11-26 01:03:35 +0000

    add formik field boiler plate

---
+++ src/components/FormikTypeAhead/FormikTypeAheadField.tsx
@@ -0,0 +1,22 @@
+import TextInput from '@vanarama/uibook/lib/components/atoms/textinput';
+import { ITextInputProps } from '@vanarama/uibook/lib/components/atoms/textinput/interfaces';
+import Formgroup from '@vanarama/uibook/lib/components/molecules/formgroup';
+import { useField } from 'formik';
+import React from 'react';
+
+interface IProps extends ITextInputProps {
+  name: string;
+  label: string;
+}
+
+const FormikTypeAheadField: React.FC<IProps> = ({ name, label, ...rest }) => {
+  const [field, meta] = useField(name);
+  const error = (meta.touched && meta.error) || undefined;
+  return (
+    <Formgroup controlId={name} label={label} error={error}>
+      <TextInput dataTestId={name} id={name} {...rest} {...field} />
+    </Formgroup>
+  );
+};
+
+export default FormikTypeAheadField;
