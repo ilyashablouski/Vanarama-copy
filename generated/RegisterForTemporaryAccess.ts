2481238e59ff28712a402c94be83847547a24f0e
Anton Adamkovich <a.adamkovich@reply.com>
2020-12-01 20:00:11 +0300

    Add uuid for email at about B2B/B2C pages

--- generated/RegisterForTemporaryAccess.ts
+++ generated/RegisterForTemporaryAccess.ts
@@ -7,8 +7,16 @@
 // GraphQL mutation operation: RegisterForTemporaryAccess
 // ====================================================
 
+export interface RegisterForTemporaryAccess_registerForTemporaryAccess_emailAddress {
+  uuid: string | null;
+  value: string | null;
+  kind: string | null;
+}
+
 export interface RegisterForTemporaryAccess_registerForTemporaryAccess {
   accessToken: string | null;
+  isSuccessfull: boolean | null;
+  emailAddress: RegisterForTemporaryAccess_registerForTemporaryAccess_emailAddress | null;
 }
 
 export interface RegisterForTemporaryAccess {
