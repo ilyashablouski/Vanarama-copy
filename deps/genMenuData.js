2978e5c1758a6495da47f234300bed85c5d702ec
street2geek <screenlantern@gmail.com>
2020-12-07 23:09:45 +0000

    check dir exist

--- deps/genMenuData.js
+++ deps/genMenuData.js
@@ -43,24 +43,25 @@ const query = gql`
 `;
 // Arg access to nextConfig object
 module.exports = async () => {
+  const dir = path.resolve('deps/data');
   const client = new GraphQLClient(process.env.API_URL, query, {
     headers: { 'x-api-key': process.env.API_KEY },
   });
 
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+
   try {
     const menuData = await client.request(query);
-    fs.writeFile(
-      path.resolve('deps/data/menuData.json'),
-      JSON.stringify(menuData),
-      err => {
-        if (err) {
-          throw new Error(err);
-        }
-        console.log(
-          'Contentful Primary Navigation JSON data file was generated!',
-        );
-      },
-    );
+    fs.writeFile(`${dir}/menuData.json`, JSON.stringify(menuData), err => {
+      if (err) {
+        throw new Error(err);
+      }
+      console.log(
+        'Contentful Primary Navigation JSON data file was generated!',
+      );
+    });
   } catch (err) {
     console.error(err);
   }
