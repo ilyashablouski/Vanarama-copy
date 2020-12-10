eba50f97fe515cd843ae90c6cb273f2390cdd1a3
street2geek <screenlantern@gmail.com>
2020-12-10 09:52:29 +0000

    amend

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -43,7 +43,6 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {/* <RollbarScript /> */}
-          <GTMDataLayerScript />
           {gtmEnvs.includes(env) && <GTMDataLayerScript />}
           {gtmEnvs.includes(env) && <GTMScript />}
           {vwoEnvs.includes(env) && <VWOScript />}
