860314fa78a579a5f46ac95fb354af9756063547
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2020-12-12 23:46:13 +0200

    Fix lint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,37 +12,6 @@ const NextScript = dynamic(() =>
   import('next/document').then(mod => mod.NextScript),
 );
 
-// @ts-ignore
-const GTMScript = dynamic(() =>
-  import('../components/GTM').then(mod => mod.Script),
-);
-// @ts-ignore
-const GTMBody = dynamic(() =>
-  import('../components/GTM').then(mod => mod.Body),
-);
-// @ts-ignore
-const GTMDataLayerScript = dynamic(() =>
-  import('../components/GTM').then(mod => mod.DataLayer),
-);
-
-// @ts-ignore
-const VWOScript = dynamic(() =>
-  import('../components/VWO').then(mod => mod.Script),
-);
-
-// @ts-ignore
-const SpeedCurveScript = dynamic(() =>
-  import('../components/SpeedCurve').then(mod => mod.Script),
-);
-
-const env = process?.env?.ENV || '';
-
-// GTM environments.
-const gtmEnvs = ['uat'];
-
-// VWO environments.
-const vwoEnvs = ['uat'];
-
 class MyDocument extends Document {
   render() {
     return (
