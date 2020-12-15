c879dd2579f8f4e48c2ee14c3d7ff6662a36ef8e
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2020-12-15 11:40:03 +0200

    Update ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     'prettier/@typescript-eslint',
     'plugin:testing-library/react',
   ],
-  plugins: ["react-hooks"],
+  plugins: ['react-hooks'],
   parserOptions: {
     project: './tsconfig.json',
   },
@@ -15,15 +15,16 @@ module.exports = {
     'react/react-in-jsx-scope': 0,
     'react/prop-types': 0,
     'react/static-property-placement': 0,
-    "jsx-a11y/anchor-is-valid": [ "error", { "components": ["a"] }],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "no-underscore-dangle": ["error", { "allow": ["__typename"] }],
-    "@typescript-eslint/no-use-before-define": ["error", { "functions": false }],
-    "testing-library/no-manual-cleanup": "error",
-    "testing-library/prefer-screen-queries": "error",
-    "testing-library/prefer-wait-for": "error",
-    "testing-library/prefer-presence-queries": "error"
+    'jsx-a11y/anchor-is-valid': ['error', { components: ['a'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-underscore-dangle': ['error', { allow: ['__typename'] }],
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+    'testing-library/no-manual-cleanup': 'error',
+    'testing-library/prefer-screen-queries': 'error',
+    'testing-library/prefer-wait-for': 'error',
+    'testing-library/prefer-presence-queries': 'error',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   },
   overrides: [
     {
@@ -31,7 +32,7 @@ module.exports = {
       rules: {
         'react/jsx-props-no-spreading': 0,
         'react/state-in-constructor': 0,
-        "react/react-in-jsx-scope": 0
+        'react/react-in-jsx-scope': 0,
       },
     },
   ],
