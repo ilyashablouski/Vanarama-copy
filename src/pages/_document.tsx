5ce4741029582126768a5e25844c8c131f870020
iamgnlc <gianluca.agnocchetti@autorama.co.uk>
2020-12-15 11:36:14 +0000

    font preload, search performance improvements

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,7 +28,10 @@ const GTMDataLayerScript = dynamic(() =>
 const env = process?.env?.ENV || '';
 
 // GTM environments.
-const gtmEnvs = ['uat'];
+const gtmEnvs = ['uat', 'pre-prod', 'prod'];
+
+// BlueConic environments.
+// const bcEnvs = ['uat', 'pre-prod', 'prod'];
 
 // VWO environments.
 // const vwoEnvs = ['uat'];
@@ -38,10 +41,12 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <HeadCustom>
-          <script src="https://cdn.blueconic.net/vanarama.js" />
-          {/* <RollbarScript /> */}
+          {/* {bcEnvs.includes(env) && ( */}
+          <script async src="https://cdn.blueconic.net/vanarama.js" />
+          {/* )} */}
           {gtmEnvs.includes(env) && <GTMDataLayerScript />}
           {gtmEnvs.includes(env) && <GTMScript />}
+          {/* <RollbarScript /> */}
         </HeadCustom>
         <body>
           <Main />
