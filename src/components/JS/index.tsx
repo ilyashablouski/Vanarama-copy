08d1106af179912730501491a1b770f6126ba429
iamgnlc <gianluca.agnocchetti@autorama.co.uk>
2020-12-22 17:22:48 +0000

    dynamic external js

---
+++ src/components/JS/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const JS = () => (
+  <>
+    <script defer src="//cdn.embedly.com/widgets/platform.js" charSet="UTF-8" />
+    <script defer src="https://www.riddle.com/files/js/embed.js" />
+  </>
+);
+export default React.memo(JS);
