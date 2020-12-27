534cb56a9023aa4b0a31a8da5110890495ec9b6b
iamgnlc <gianluca.agnocchetti@autorama.co.uk>
2020-12-27 15:59:10 +0000

    reverted maxsize

--- config/app.js
+++ config/app.js
@@ -105,7 +105,7 @@ module.exports = {
           ...config.optimization.splitChunks,
           chunks: 'all',
           // minSize: 10000,
-          maxSize: 150000,
+          maxSize: 120000,
           maxAsyncRequests: 100,
           maxInitialRequests: 100,
           cacheGroups: {
