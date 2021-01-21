dfdaef9874f2c28cf885ad1601e982da184d073c
Anton Adamkovich <a.adamkovich@reply.com>
2021-01-21 15:53:07 +0300

    Add fetch polity to imperative query

--- src/hooks/useImperativeQuery.ts
+++ src/hooks/useImperativeQuery.ts
@@ -1,6 +1,6 @@
 import {
   DocumentNode,
-  OperationVariables,
+  OperationVariables, QueryOptions,
   useApolloClient,
 } from '@apollo/client';
 import React from 'react';
@@ -8,13 +8,17 @@ import React from 'react';
 export function useImperativeQuery<
   TData = any,
   TVariables = OperationVariables
->(query: DocumentNode) {
+>(
+  query: DocumentNode,
+  fetchPolicy: QueryOptions['fetchPolicy'] = 'network-only',
+) {
   const client = useApolloClient();
   return React.useCallback(
     (variables: TVariables) =>
       client.query<TData, TVariables>({
         query,
         variables,
+        fetchPolicy,
       }),
     [client, query],
   );
