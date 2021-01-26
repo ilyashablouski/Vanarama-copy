920a2361cf0741cf286224d1e3dc0369f117b4a3
iamgnlc <gianluca.agnocchetti@gmail.com>
2021-01-26 15:20:37 +0000

    removed script

--- src/components/JS/index.tsx
+++ src/components/JS/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 const JS = () => (
   <>
-    <script defer src="//cdn.embedly.com/widgets/platform.js" charSet="UTF-8" />
     <script defer src="https://www.riddle.com/files/js/embed.js" />
   </>
 );
