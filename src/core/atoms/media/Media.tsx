918e4d7d18f33b69b3047a3683895820a0f964fb
iamgnlc <gianluca.agnocchetti@gmail.com>
2021-02-10 10:41:48 +0000

    removed lazy load from Media and direct imported

--- src/core/atoms/media/Media.tsx
+++ src/core/atoms/media/Media.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactPlayer from 'react-player/lazy';
-import { LazyLoadComponent } from 'react-lazy-load-image-component';
+// import { LazyLoadComponent } from 'react-lazy-load-image-component';
 import cx from 'classnames';
 import { IMediaProps } from './interface';
 
@@ -21,29 +21,29 @@ const Media: React.FC<IMediaProps> = ({
   iframe,
 }) => (
   <div className={cx('media', className)}>
-    <LazyLoadComponent visibleByDefault={typeof window === 'undefined'}>
-      {iframe ? (
-        <div className={cx({ 'media-player--embed': responsive })}>
-          <iframe width={width} height={height} src={src} title="media" />
-        </div>
-      ) : (
-        <ReactPlayer
-          className={cx({ 'media-player--embed': responsive })}
-          url={src}
-          playing={playing}
-          src={src}
-          width={width}
-          height={height}
-          loop={loop}
-          controls={controls}
-          light={light}
-          volume={volume}
-          muted={muted}
-          data-testid={dataTestId}
-          config={{ vimeo: { playerOptions: vimeoConfig } }}
-        />
-      )}
-    </LazyLoadComponent>
+    {/* <LazyLoadComponent visibleByDefault={typeof window === 'undefined'}> */}
+    {iframe ? (
+      <div className={cx({ 'media-player--embed': responsive })}>
+        <iframe width={width} height={height} src={src} title="media" />
+      </div>
+    ) : (
+      <ReactPlayer
+        className={cx({ 'media-player--embed': responsive })}
+        url={src}
+        playing={playing}
+        src={src}
+        width={width}
+        height={height}
+        loop={loop}
+        controls={controls}
+        light={light}
+        volume={volume}
+        muted={muted}
+        data-testid={dataTestId}
+        config={{ vimeo: { playerOptions: vimeoConfig } }}
+      />
+    )}
+    {/* </LazyLoadComponent> */}
   </div>
 );
 
