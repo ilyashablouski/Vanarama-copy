929d3b370ad10e40b54ef20e17e2979176ca5bb1
iamgnlc <gianluca.agnocchetti@gmail.com>
2021-02-11 09:00:18 +0000

    added default font

--- src/components/Head/fonts.js
+++ src/components/Head/fonts.js
@@ -3,19 +3,21 @@ const FONT_PATH = `${process.env.STATIC_DOMAIN}/fonts/`;
 const FONT_LIST = [
   // 'DkMono/DkMono-400-normal.otf',
   // 'DkMono/DkMono-400-italic.otf',
-  'FFCocon/FFCocon-300-normal.otf',
-  // 'FFCocon/FFCocon-300-italic.otf',
+
+  // 'FFCocon/FFCocon-300-normal.otf',
   'FFCocon/FFCocon-400-normal.otf',
+
+  // 'NunitoSans/NunitoSans-300-normal.otf',
+  // 'NunitoSans/NunitoSans-400-normal.otf',
+  // 'NunitoSans/NunitoSans-500-normal.otf',
+  // 'NunitoSans/NunitoSans-600-normal.otf',
+
   // 'FFCocon/FFCocon-400-italic.otf',
   // 'NunitoSans/NunitoSans-200-normal.otf',
   // 'NunitoSans/NunitoSans-200-italic.otf',
-  'NunitoSans/NunitoSans-300-normal.otf',
   // 'NunitoSans/NunitoSans-300-italic.otf',
-  'NunitoSans/NunitoSans-400-normal.otf',
   // 'NunitoSans/NunitoSans-400-italic.otf',
-  'NunitoSans/NunitoSans-500-normal.otf',
   // 'NunitoSans/NunitoSans-500-italic.otf',
-  'NunitoSans/NunitoSans-600-normal.otf',
   // 'NunitoSans/NunitoSans-600-italic.otf',
   // 'NunitoSans/NunitoSans-700-normal.otf',
   // 'NunitoSans/NunitoSans-700-italic.otf',
