7cc8f1b15debdcd535b358b092cbeae6598801c8
iamgnlc <gianluca.agnocchetti@gmail.com>
2021-02-16 12:19:22 +0000

    fixed body style icons

--- src/utils/bodyStyleIconMap.tsx
+++ src/utils/bodyStyleIconMap.tsx
@@ -1,31 +1,27 @@
+import CityCar from '../core/assets/icons/custom/CityCar';
 import Convertible from '../core/assets/icons/custom/Convertible';
-import Coupe from '../core/assets/icons/custom/Coupe';
-import Eco from '../core/assets/icons/custom/Eco';
+import Coupe from '../core/assets/icons/custom/_Coupe';
 import Estate from '../core/assets/icons/custom/Estate';
-import Family from '../core/assets/icons/custom/Family';
 import Hatchback from '../core/assets/icons/custom/Hatchback';
 import PeopleCarrier from '../core/assets/icons/custom/PeopleCarrier';
-import Pickups from '../core/assets/icons/custom/Pickups';
-import Prestige from '../core/assets/icons/custom/Prestige';
 import Saloon from '../core/assets/icons/custom/Saloon';
 import Small from '../core/assets/icons/custom/Small';
 import SUV from '../core/assets/icons/custom/SUV';
-import Vans from '../core/assets/icons/custom/Vans';
+// import Pickups from '../core/assets/icons/custom/Pickups';
+// import Vans from '../core/assets/icons/custom/Vans';
 
 const BodyStyleIconMap = new Map();
 
 BodyStyleIconMap.set('Convertible', <Convertible />)
+  .set('4x4', <SUV />)
+  .set('CityCar', <CityCar />)
   .set('Coupe', <Coupe />)
-  .set('Eco', <Eco />)
   .set('Estate', <Estate />)
-  .set('Family', <Family />)
   .set('Hatchback', <Hatchback />)
   .set('PeopleCarrier', <PeopleCarrier />)
-  .set('Pickups', <Pickups />)
-  .set('Prestige', <Prestige />)
   .set('Saloon', <Saloon />)
-  .set('Small', <Small />)
-  .set('4x4', <SUV />)
-  .set('Vans', <Vans />);
+  .set('Small', <Small />);
+// .set('Pickups', <Pickups />)
+// .set('Vans', <Vans />)
 
 export default BodyStyleIconMap;
