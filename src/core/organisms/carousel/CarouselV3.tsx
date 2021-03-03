b5c0c7bc2c8abce2a734bf1fb14c29e342b7e21b
nasiraltaf78 <nasiraltaf78@gmail.com>
2021-03-03 15:20:44 +0000

    Re-implemented carousel card height fix

--- src/core/organisms/carousel/CarouselV3.tsx
+++ src/core/organisms/carousel/CarouselV3.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import cx from 'classnames';
 import Carousel from 'nuka-carousel';
 import { useMediaQuery } from 'react-responsive';
@@ -22,6 +22,15 @@ const Slider: FC<ICarouselProps> = ({ children, className, countItems }) => {
     slidesToShow = 2;
   }
 
+  useEffect(() => {
+    setTimeout(() => {
+      const sliders = document.querySelectorAll('.carousel .card');
+      Array.from(Array(sliders.length)).forEach((el, id) => {
+        (sliders[id] as HTMLElement).className += ' -v-height';
+      });
+    }, 200);
+  }, []);
+
   return (
     <div className={cx('carousel', className)}>
       <Carousel
@@ -37,7 +46,8 @@ const Slider: FC<ICarouselProps> = ({ children, className, countItems }) => {
         afterSlide={(slideIndex: number) => setIndex(slideIndex)}
         cellSpacing={20}
         initialSlideWidth={300}
-        initialSlideHeight={575}
+        // initialSlideHeight={575}
+        //heightMode="max"
       >
         {children}
       </Carousel>
