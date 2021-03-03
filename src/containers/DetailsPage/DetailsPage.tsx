f578be118756dc003df193f99f92f43f5a26c128
iamgnlc <gianluca.agnocchetti@gmail.com>
2021-03-03 20:23:30 +0000

    added space

--- src/containers/DetailsPage/DetailsPage.tsx
+++ src/containers/DetailsPage/DetailsPage.tsx
@@ -326,7 +326,7 @@ const DetailsPage: React.FC<IDetailsPageProps> = ({
   const pageTitle = `${vehicleConfigurationByCapId?.capManufacturerDescription} ${vehicleConfigurationByCapId?.capModelDescription}`;
 
   // eslint-disable-next-line no-console
-  if (process.env.ENV !== 'production') console.log('CAP Id:', capId);
+  if (process.env.ENV !== 'prod') console.log('CAP Id:', capId);
 
   const onSubmitClickMobile = () => {
     const colourDescription = derivativeInfo?.colours?.find(
@@ -426,7 +426,7 @@ const DetailsPage: React.FC<IDetailsPageProps> = ({
         <Heading tag="h1">
           <Heading className="-pt-100" tag="span" size="xlarge" color="black">
             {pageTitle}
-          </Heading>
+          </Heading>{' '}
           <Text tag="span" size="lead" color="darker">
             {vehicleConfigurationByCapId?.capDerivativeDescription}
           </Text>
