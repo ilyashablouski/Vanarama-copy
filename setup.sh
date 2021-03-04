b2516e937f33ae6d0330c7e32ebcfe744f68219f
Vedprakash Upraity <vedprakashupraity@gmail.com>
2021-03-05 00:08:12 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ REGION=${4}
 CURRENTBRANCH=${5}
 ALTERNATEDOMAIN=${6}
 HOST_DOMAIN="https://${6}"
+IMGOPTIMISATIONHOST=${7}
 
 export ENV=$ENV
 export API_KEY="$(aws ssm get-parameter --name "/$ENV/$STACK/$APP/fed-gateway-api-key" --region $REGION --with-decryption | jq -r ".Parameter.Value")"
@@ -14,7 +15,7 @@ export LOQATE_KEY="$(aws ssm get-parameter --name "/$ENV/$STACK/$APP/loqate-key"
 export ROLLBAR_CLIENT_TOKEN="$(aws ssm get-parameter --name "/$ENV/$STACK/$APP/rollbar-client-token" --region $REGION --with-decryption | jq -r ".Parameter.Value")"
 export ROLLBAR_SERVER_TOKEN="$(aws ssm get-parameter --name "/$ENV/$STACK/$APP/rollbar-server-token" --region $REGION --with-decryption | jq -r ".Parameter.Value")"
 export GTM_ID="$(aws ssm get-parameter --name "/$ENV/$STACK/$APP/gtm-id" --region $REGION --with-decryption | jq -r ".Parameter.Value")"
-export IMG_OPTIMISATION_HOST="https://www.$ALTERNATEDOMAIN"
+export IMG_OPTIMISATION_HOST="$IMGOPTIMISATIONHOST"
 export PRERENDER_SERVICE_URL="https://prerender-service-production.herokuapp.com/"
 export MICROBLINK_URL="https://microblink-secure.motorama.com"
 export STATIC_DOMAIN="$(aws ssm get-parameter --name "/$ENV/$STACK/$APP/static-domain" --region $REGION --with-decryption | jq -r ".Parameter.Value")"
