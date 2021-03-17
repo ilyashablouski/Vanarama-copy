e52627cd28fce199f5aaf4a0daef093a0dbc4568
“Jon <jonrogers121@gmail.com>
2021-03-17 14:53:39 +0000

    removed space

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -44,7 +44,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <HeadCustom>
           {/* <RollbarScript /> */}
-          {/* <link rel="preload" href="/styles/base.css" as="style" /> 
+          {/* <link rel="preload" href="/styles/base.css" as="style" />
           <link rel="stylesheet" href="/styles/base.css" /> */}
           {scriptEnvs.blueconic.includes(env) && (
             <script async src="https://cdn.blueconic.net/vanarama.js" />
