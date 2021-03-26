3fc20106eb3cf4af91f6ffd3873048a134bf6acb
“Jon <jonrogers121@gmail.com>
2021-03-26 12:46:35 +0000

    Chaned lint issues

--- src/graphQlTests/index.js
+++ src/graphQlTests/index.js
@@ -1,12 +1,12 @@
 const blogPosts = require('./blogPosts');
 
-var delay = 1000;
+let delay = 1000;
 
 function runQuery(callBack) {
   setTimeout(function() {
     callBack();
   }, delay);
-  delay = delay + 1000;
+  delay += 1000;
 }
 
 runQuery(blogPosts);
