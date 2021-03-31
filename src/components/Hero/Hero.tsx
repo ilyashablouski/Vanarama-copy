3e13035ee71d92cd798a32c8f9d34761da557133
nasiraltaf78 <nasiraltaf78@gmail.com>
2021-03-31 13:21:00 +0100

    Added code comment

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -95,6 +95,8 @@ const Hero: React.FC<IHeroProps> = ({
       <div className="row:hero">
         <div className="hero--left">{children}</div>
         <div className="hero--right" style={{ minHeight: '347px' }}>
+          {/* NOTE: Some components using dynamic imports are causing issues affecting next sibling CSS classnames 
+              from rendering as expected. This issue is happening when rehydrating on the client-side */}
           {renderHeroRight()}
           <BenefitsBar countItems={4} />
         </div>
