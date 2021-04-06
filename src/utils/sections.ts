ed7bf5428d521afb96ec52611e3453658f6dc96b
“Jon <jonrogers121@gmail.com>
2021-04-06 12:44:42 +0100

    added section key

--- src/utils/sections.ts
+++ src/utils/sections.ts
@@ -1,8 +1,8 @@
 // eslint-disable-next-line import/prefer-default-export
 export const getFeaturedSectionsAsArray = (sectionObject: any) => {
   const featuresArray: any = [];
-  const sectionKeys = Object.keys(sectionObject).filter(x =>
-    x.includes('featured'),
+  const sectionKeys = Object.keys(sectionObject).filter(key =>
+    key.includes('featured'),
   );
   sectionKeys.forEach(elem => {
     if (sectionObject[elem]) featuresArray.push(sectionObject[elem]);
