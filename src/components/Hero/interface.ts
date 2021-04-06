4212c7dad45822ab038f36e4afbff790a1ef0a48
“Jon <jonrogers121@gmail.com>
2021-04-06 09:18:29 +0100

    Changes

--- src/components/Hero/interface.ts
+++ src/components/Hero/interface.ts
@@ -7,7 +7,6 @@ export interface IHeroProps {
   workingHoursCard?: WorkingHoursCard;
   searchPodVansData?: IFilterList;
   searchPodCarsData?: IFilterList;
-  showImageonRight?: boolean;
 }
 
 export interface IHeroTitleProps {
