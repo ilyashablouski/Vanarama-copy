0091f3dfb19dcbd79b093a7fa14a20ba7917a689
“Jon <jonrogers121@gmail.com>
2021-04-09 12:00:20 +0100

    Changed to screen

--- src/components/NewLeaseOfLifePriceHeader/NewLeaseOfLifePriceHeader.test.tsx
+++ src/components/NewLeaseOfLifePriceHeader/NewLeaseOfLifePriceHeader.test.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import NewLeaseOfLifePriceHeader from '.';
 
 describe('NewLeaseOfLifeHeader', () => {
   test('Renders the title', async () => {
-    const { getByText, getByTestId } = render(
+    render(
       <NewLeaseOfLifePriceHeader
         title="Tesla Model 3 Saloon"
-        body="From £423+ vat per month"
+        body="From £423 + vat per month"
       />,
     );
-    expect(getByText(/Tesla Model 3 Saloon/i)).toBeInTheDocument();
+    expect(screen.getByText(/Tesla Model 3 Saloon/i)).toBeInTheDocument();
   });
   test('Renders the price in a span tag', async () => {
-    const { getByTestId } = render(
+    render(
       <NewLeaseOfLifePriceHeader
         title="Tesla Model 3 Saloon"
         body="From £423+ vat per month"
       />,
     );
-    expect(getByTestId('price')).toHaveTextContent(/423/i);
+    expect(screen.getByTestId('price')).toHaveTextContent(/423/i);
   });
 });
