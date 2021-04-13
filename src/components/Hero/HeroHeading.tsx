42b8ac0ce161e18dcd118fa5e77b63d482795a4f
Wafiq Baugh <screenlantern@gmail.com>
2021-04-14 00:53:50 +0100

    bulk changes

--- src/components/Hero/HeroHeading.tsx
+++ src/components/Hero/HeroHeading.tsx
@@ -4,13 +4,17 @@ import getTitleTag from '../../utils/getTitleTag';
 import { IHeroHeadingProps } from './interface';
 import Skeleton from '../Skeleton';
 
-const HeroHeading: React.FC<IHeroHeadingProps> = ({ text, titleTag }) => {
+const HeroHeading: React.FC<IHeroHeadingProps> = ({
+  text,
+  titleTag,
+  color = 'white',
+}) => {
   if (!text) return <Skeleton count={2} />;
 
   return (
     <Heading
       size="xlarge"
-      color="white"
+      color={color}
       tag={getTitleTag(titleTag || 'h1') as keyof JSX.IntrinsicElements}
     >
       {text}
--- src/utils/layout.ts
+++ src/utils/layout.ts
@@ -1,10 +1,8 @@
 // eslint-disable-next-line import/prefer-default-export
-export function getFeaturedClassPartial(featured: any) {
-  if (featured?.layout?.includes('Full Width')) {
+export function getFeaturedClassPartial(layout: any) {
+  if (layout?.includes('Full Width')) {
     return 'featured-fullwidth';
   }
 
-  return featured?.layout?.includes('Media Left')
-    ? 'featured-left'
-    : 'featured-right';
+  return layout?.includes('Media Left') ? 'featured-left' : 'featured-right';
 }
