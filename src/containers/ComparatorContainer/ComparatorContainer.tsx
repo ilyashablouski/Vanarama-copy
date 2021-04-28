0ed468ac424fe52c03a9d3dae9688e9967bc3343
Anton Adamkovich <a.adamkovich@reply.com>
2021-04-28 23:47:13 +0300

    Add modal on car adding

--- src/containers/ComparatorContainer/ComparatorContainer.tsx
+++ src/containers/ComparatorContainer/ComparatorContainer.tsx
@@ -1,5 +1,5 @@
 import dynamic from 'next/dynamic';
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState, useContext, useCallback } from 'react';
 import ComparatorTable from 'core/organisms/comparator-table';
 import Router from 'next/router';
 import { CompareContext, INIT_VEHICLE } from '../../utils/comparatorTool';
@@ -13,7 +13,20 @@ import { VehicleTypeEnum } from '../../../generated/globalTypes';
 import Skeleton from '../../components/Skeleton';
 import useLeaseType from '../../hooks/useLeaseType';
 
+import pickUpImage from '/public/Assets/images/comparator/modal/cap-44067-160978.png';
+import vanImage from '/public/Assets/images/comparator/modal/cap-51392-171678.png';
+import carImage from '/public/Assets/images/comparator/modal/cap-88928-161019.png';
+
 const Loading = dynamic(() => import('core/atoms/loading'), {
+  loading: () => <Skeleton count={1} />,
+});
+const Modal = dynamic(() => import('core/molecules/modal'), {
+  loading: () => <Skeleton count={1} />,
+});
+const Heading = dynamic(() => import('core/atoms/heading'), {
+  loading: () => <Skeleton count={1} />,
+});
+const Card = dynamic(() => import('core/molecules/cards/Card'), {
   loading: () => <Skeleton count={1} />,
 });
 
@@ -21,6 +34,7 @@ const ComparatorContainer: React.FC = () => {
   const [vehicles, setVehicles] = useState<
     { capId: number; vehicleType: VehicleTypeEnum | null }[]
   >([]);
+  const [isModalVisible, setModalVisibility] = useState(false);
   const { compareVehicles, compareChange } = useContext(CompareContext);
   const { cachedLeaseType } = useLeaseType(null);
 
@@ -40,6 +54,14 @@ const ComparatorContainer: React.FC = () => {
       refetch();
     }
   }, [vehicles, compareVehicles, refetch]);
+
+  const handleVehicleAdd = useCallback(() => {
+    if ((compareVehicles || []).length > 0) {
+      Router.back();
+    } else {
+      setModalVisibility(true);
+    }
+  }, [compareVehicles]);
 
   if (error) {
     return (
@@ -72,23 +94,56 @@ const ComparatorContainer: React.FC = () => {
   };
 
   return (
-    <ComparatorTable
-      addVehicle={() => {
-        Router.back();
-      }}
-      deleteVehicle={capId => {
-        compareChange(null, capId);
-      }}
-      criterias={getCriterials(
-        (data as vehicleComparator) ?? (INIT_VEHICLE as vehicleComparator),
-        compareVehicles,
-        cachedLeaseType,
+    <>
+      <ComparatorTable
+        addVehicle={handleVehicleAdd}
+        deleteVehicle={capId => {
+          compareChange(null, capId);
+        }}
+        criterias={getCriterials(
+          (data as vehicleComparator) ?? (INIT_VEHICLE as vehicleComparator),
+          compareVehicles,
+          cachedLeaseType,
+        )}
+        viewOffer={capId => {
+          viewOffer(`${capId}`);
+        }}
+        isNotEmptyPage={!!data}
+      />
+      {isModalVisible && (
+        <Modal
+          // className="-mt-000 callBack"
+          show
+          onRequestClose={() => setModalVisibility(false)}
+        >
+          <div className="-pt-000">
+            <div>
+              <Heading tag="span" color="black">
+                Choose the type of vehicle are you looking for?
+              </Heading>
+            </div>
+            <Card inline imageSrc={vanImage}>
+              <div>
+                <div>SOME</div>
+                <div>EMOS</div>
+              </div>
+            </Card>
+            <Card imageSrc={pickUpImage}>
+              <div>
+                <div>SOME</div>
+                <div>EMOS</div>
+              </div>
+            </Card>
+            <Card imageSrc={carImage}>
+              <div>
+                <div>SOME</div>
+                <div>EMOS</div>
+              </div>
+            </Card>
+          </div>
+        </Modal>
       )}
-      viewOffer={capId => {
-        viewOffer(`${capId}`);
-      }}
-      isNotEmptyPage={!!data}
-    />
+    </>
   );
 };
 
