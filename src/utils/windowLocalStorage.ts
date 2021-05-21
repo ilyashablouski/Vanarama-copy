ee8404105a9854b6a4d382ebe38b673454c77285
“Jon <jonrogers121@gmail.com>
2021-05-21 15:38:26 +0100

    Removed any

--- src/utils/windowLocalStorage.ts
+++ src/utils/windowLocalStorage.ts
@@ -1,11 +1,11 @@
-export const setLocalStorage = (key: string, value: any) => {
-  window?.localStorage.setItem(key, value);
+export const setLocalStorage = (key: string, value: string) => {
+  window?.localStorage?.setItem(key, value);
 };
 
 export const getLocalStorage = (key: string) => {
-  return window?.localStorage.getItem(key);
+  return window?.localStorage?.getItem(key);
 };
 
 export const removeLocalStorage = (key: string) => {
-  return window?.localStorage.removeItem(key);
+  return window?.localStorage?.removeItem(key);
 };
