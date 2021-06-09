c5d275d390cbbd59e14935a6e4eaee25164e0177
galko <v.galko@agentestudio.com>
2021-06-09 13:34:04 +0300

    refactoring wishlist helper

--- src/containers/WishlistPageContainer/helpers.ts
+++ src/containers/WishlistPageContainer/helpers.ts
@@ -2,10 +2,8 @@ const MAX_PLACEHOLDER_LIST_LENGTH = 3;
 
 // eslint-disable-next-line import/prefer-default-export
 export function getProductPlaceholderList(productListLength: number) {
-  const productPlaceholderListLength =
-    MAX_PLACEHOLDER_LIST_LENGTH - productListLength;
   const resultProductPlaceholderListLength =
-    productPlaceholderListLength > 0 ? productPlaceholderListLength : 0;
+    MAX_PLACEHOLDER_LIST_LENGTH - productListLength;
 
   return Array.from(
     { length: resultProductPlaceholderListLength },
