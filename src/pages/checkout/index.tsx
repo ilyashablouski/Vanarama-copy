b101b16b62d7dce872a6a160aa684f1a809da44f
AntonAdamkovich <anton.adamkovich@gmail.com>
2021-06-17 11:15:59 +0300

    Hide new checkout with feature cookies

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,11 +1,12 @@
-import { NextPage } from 'next';
 import React from 'react';
+import Cookies from 'js-cookie';
+import { NextPage } from 'next';
 
 import Loading from 'core/atoms/loading';
-
 import CheckoutPageContainer from '../../containers/CheckoutPageContainer';
 import useGetOrder from '../../hooks/useGetOrder';
 import { useCarDerivativeQuery } from '../../gql/order';
+import PageNotFoundContainer from '../../containers/PageNotFoundContainer/PageNotFoundContainer';
 
 const CheckoutPage: NextPage = () => {
   const order = useGetOrder();
@@ -15,6 +16,10 @@ const CheckoutPage: NextPage = () => {
     vehicleProduct?.derivativeCapId,
     vehicleProduct?.vehicleType,
   );
+
+  if (Cookies.get('DIG-6240') !== '1') {
+    return <PageNotFoundContainer name="Checkout" />;
+  }
 
   if (loading || !order) {
     return <Loading size="xlarge" />;
