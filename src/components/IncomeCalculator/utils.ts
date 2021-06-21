3d180aac7cd4f00ad4f0e4c888861e20353ad00b
AntonAdamkovich <anton.adamkovich@gmail.com>
2021-06-21 17:45:37 +0300

    Rework expenses validation

--- src/components/IncomeCalculator/utils.ts
+++ src/components/IncomeCalculator/utils.ts
@@ -1,18 +1,29 @@
 import { IIncomeCalculatorFormValues } from './interfaces';
+import { IncomeCalculatorExpenditure } from '../../../generated/IncomeCalculatorExpenditure';
+import { sum } from '../../utils/array';
+
+export const calculateMonthlyExpenses = (
+  values?: IIncomeCalculatorFormValues | IncomeCalculatorExpenditure | null,
+) =>
+  sum(
+    [
+      values?.mortgageOrRent,
+      values?.phoneAndInternet,
+      values?.creditCardPayments,
+      values?.insurance,
+      values?.foodAndClothes,
+      values?.studentLoan,
+      values?.utilities,
+      values?.carFinance,
+      values?.fuel,
+      values?.otherCredit,
+    ],
+    item => Number(item || ''),
+  );
 
 // eslint-disable-next-line import/prefer-default-export
 export const calculateIncome = (values: IIncomeCalculatorFormValues) => {
-  const monthlyExpenses =
-    Number(values.mortgageOrRent) +
-    Number(values.phoneAndInternet) +
-    Number(values.creditCardPayments) +
-    Number(values.insurance) +
-    Number(values.foodAndClothes) +
-    Number(values.studentLoans) +
-    Number(values.utilities) +
-    Number(values.carFinance) +
-    Number(values.fuel) +
-    Number(values.otherCredit);
+  const monthlyExpenses = calculateMonthlyExpenses(values);
 
   const disposableIncome =
     Number(values.averageMonthlyIncome) - monthlyExpenses;
