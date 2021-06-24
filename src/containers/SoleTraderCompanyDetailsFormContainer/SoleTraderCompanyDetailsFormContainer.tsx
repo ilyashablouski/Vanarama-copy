dd1893da0a1c538ee2c56cfd70349c79ea7cc9fc
galko <v.galko@agentestudio.com>
2021-06-24 17:49:07 +0300

    fix lint

--- src/containers/SoleTraderCompanyDetailsFormContainer/SoleTraderCompanyDetailsFormContainer.tsx
+++ src/containers/SoleTraderCompanyDetailsFormContainer/SoleTraderCompanyDetailsFormContainer.tsx
@@ -1,5 +1,5 @@
 import dynamic from 'next/dynamic';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import CompanyDetailsForm from '../../components/SoleTraderCompanyDetailsForm';
 import { ISoleTraderCompanyDetailsFormValues } from '../../components/SoleTraderCompanyDetailsForm/interfaces';
 import { ISoleTraderCompanyDetailsFormContainerProps } from './interfaces';
