e4ff36d893e64afa25d85a9dc7901cc58822e0d1
galko <v.galko@agentestudio.com>
2021-07-14 15:37:32 +0300

    fix lint

--- wdyr.js
+++ wdyr.js
@@ -8,6 +8,7 @@ import React from 'react';
 
 if (process.env.NODE_ENV === 'development') {
   if (typeof window !== 'undefined') {
+    // eslint-disable-next-line global-require
     const whyDidYouRender = require('@welldone-software/why-did-you-render');
 
     whyDidYouRender(React, {
