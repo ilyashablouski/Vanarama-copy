c2f631083051488693f28e03bca0efe3bec9eeb6
galko <v.galko@agentestudio.com>
2021-07-14 18:08:15 +0300

    move wdyr script to utils

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 // import 'core/styles/base.scss';
-import '../../wdyr';
+import 'utils/wdyr';
 import dynamic from 'next/dynamic';
 import { AppProps } from 'next/app';
 import Router from 'next/router';
