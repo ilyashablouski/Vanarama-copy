0475d707533a7e40215d625853fa4c4849494646
galko <v.galko@agentestudio.com>
2021-07-14 15:32:59 +0300

    implement why-did-you-render integration

---
+++ wdyr.js
@@ -0,0 +1,17 @@
+/**
+ * WDYR (why-did-you-render) helps locate unnecessary re-renders.
+ *
+ * @see https://github.com/welldone-software/why-did-you-render
+ * @see https://github.com/vercel/next.js/tree/canary/examples/with-why-did-you-render
+ */
+import React from 'react';
+
+if (process.env.NODE_ENV === 'development') {
+  if (typeof window !== 'undefined') {
+    const whyDidYouRender = require('@welldone-software/why-did-you-render');
+
+    whyDidYouRender(React, {
+      trackAllPureComponents: false,
+    });
+  }
+}
