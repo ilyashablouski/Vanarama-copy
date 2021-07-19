769bab7c4e03eeb61db85f7935eaad556a48407d
“Jon <jonrogers121@gmail.com>
2021-07-19 13:40:44 +0100

    Undefined issue

--- src/components/Partnerships/helpers.ts
+++ src/components/Partnerships/helpers.ts
@@ -3,6 +3,6 @@ import { PartnershipsLinks } from './Data/PartnishipLinks';
 // eslint-disable-next-line import/prefer-default-export
 export const getPartnershipLinks = (vehicleTypes: string[]) => {
   return PartnershipsLinks.filter(({ label }) =>
-    vehicleTypes.map(type => type.toUpperCase()).includes(label),
+    vehicleTypes?.map(type => type.toUpperCase()).includes(label),
   );
 };
