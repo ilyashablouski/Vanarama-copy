67bc187bd1312478a251cdd1ddb59ac806b12292
vadzim.hurchanka <vadim.gur@twinslash.com>
2021-07-21 17:54:15 +0300

    add interface component TreeColumnSection

--- src/components/ThreeColumnSection/ThreeColumnSection.tsx
+++ src/components/ThreeColumnSection/ThreeColumnSection.tsx
@@ -3,9 +3,14 @@ import Image from 'core/atoms/image/Image';
 import { getSectionsData } from '../../utils/getSectionsData';
 
 type ThreeColumnSectionProps = {
-  cards: [];
+  cards: CardItem[];
   title: string;
 };
+
+interface CardItem {
+  image: object;
+  body: string;
+}
 
 const ThreeColumnSection: React.FC<ThreeColumnSectionProps> = ({
   title,
@@ -15,20 +20,23 @@ const ThreeColumnSection: React.FC<ThreeColumnSectionProps> = ({
     return null;
   }
 
+  const defaultImage: string =
+    'https://res.cloudinary.com/diun8mklf/image/upload/c_fill,g_center,h_425,q_auto:best,w_800/v1581538982/cars/AudiQ30718_4_k5ojqt.jpg';
+
   return (
     <>
       <div className="row:default">
         <h3 className="heading -large -black -a-center -mb-500">{title}</h3>
 
         <div className="row:cards-3col -a-center -mb-500">
-          {cards?.map((item: any) => {
+          {cards?.map((item: CardItem) => {
             return (
               <>
                 <div>
                   <Image
                     src={
                       getSectionsData(['image', 'file', 'url'], item) ||
-                      'https://res.cloudinary.com/diun8mklf/image/upload/c_fill,g_center,h_425,q_auto:best,w_800/v1581538982/cars/AudiQ30718_4_k5ojqt.jpg'
+                      defaultImage
                     }
                   />
                   <p className="heading -lead -black -pv-300">{item.body}</p>
