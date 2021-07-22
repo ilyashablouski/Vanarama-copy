47ac5711b1d98076ad5e8fe64c86821a5ec2a995
Aliaksei Douhal <kcafmil@gmail.com>
2021-07-22 13:14:03 +0300

    DIG-6673 updated Modal for maintenance on PDP

---
+++ src/containers/DetailsPage/MaintenanceModalContent.tsx
@@ -0,0 +1,80 @@
+import dynamic from 'next/dynamic';
+import Ellipse from 'core/assets/icons/Ellipse';
+import EllipseOutline from 'core/assets/icons/EllipseOutline';
+import Heading from 'core/atoms/heading';
+import React from 'react';
+import { includedItems, notIncludedItems } from './config';
+import Skeleton from '../../components/Skeleton';
+
+const Text = dynamic(() => import('core/atoms/text'));
+const IconList = dynamic(() => import('core/organisms/icon-list'), {
+  loading: () => <Skeleton count={3} />,
+});
+// @ts-ignore
+const IconListItem = dynamic(() =>
+  import('core/organisms/icon-list').then(mod => mod.IconListItem),
+);
+const MaintenanceModalContent = () => {
+  return (
+    <>
+      <Text tag="p" color="dark" className="-mv-400" size="regular">
+        We know how important it is to keep you moving and keep your vehicle on
+        the road which is why we introduced the Vanarama Service Plan. The Plan
+        covers all routine servicing & maintenance but also provides full RAC
+        Breakdown Cover and a 7-day relief vehicle if you break down for a
+        hassle-free, fixed monthly charge so you don’t have to worry about a
+        thing!
+      </Text>
+
+      <Heading tag="span" color="black">
+        What’s Included?
+      </Heading>
+      <IconList className="maintenanceConditions">
+        {includedItems.map(({ value, innerItems }, index) => (
+          <IconListItem
+            iconColor="dark"
+            key={index.toString()}
+            listIcon={<Ellipse />}
+          >
+            <Text tag="span" color="dark" size="regular">
+              {value}
+              {innerItems && (
+                <IconList>
+                  {innerItems?.map((text, idx) => (
+                    <IconListItem
+                      iconColor="dark"
+                      key={idx.toString()}
+                      listIcon={<EllipseOutline />}
+                    >
+                      <Text tag="span" color="dark" size="regular">
+                        {text}
+                      </Text>
+                    </IconListItem>
+                  ))}
+                </IconList>
+              )}
+            </Text>
+          </IconListItem>
+        ))}
+      </IconList>
+      <Heading tag="span" color="black">
+        What’s Not Included?
+      </Heading>
+      <IconList className="maintenanceConditions">
+        {notIncludedItems.map((value, index) => (
+          <IconListItem
+            iconColor="dark"
+            key={index.toString()}
+            listIcon={<Ellipse />}
+          >
+            <Text tag="span" color="dark" size="regular">
+              {value}
+            </Text>
+          </IconListItem>
+        ))}
+      </IconList>
+    </>
+  );
+};
+
+export default MaintenanceModalContent;
