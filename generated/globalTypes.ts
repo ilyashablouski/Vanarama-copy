cb3490a44d9e735c8e9fe1645a36a96b89636cc5
Aliaksei Douhal <kcafmil@gmail.com>
2021-07-29 12:58:50 +0300

    DIG-6362 changed availability field for sort

--- generated/globalTypes.ts
+++ generated/globalTypes.ts
@@ -85,7 +85,7 @@ export enum ProductDerivativeSortDirection {
 }
 
 export enum ProductDerivativeSortField {
-  availability = "availability",
+  availabilitySort = "availabilitySort",
   initialPayment = "initialPayment",
   mileage = "mileage",
   offerRanking = "offerRanking",
