5bb08e1dbc0308b10d4e052afdccc1d5f2b9152e
galko <v.galko@agentestudio.com>
2021-08-05 21:36:55 +0300

    update unit test & snapshot

--- src/core/organisms/cookie-bar/__tests__/CookieBar.spec.tsx
+++ src/core/organisms/cookie-bar/__tests__/CookieBar.spec.tsx
@@ -5,7 +5,9 @@ import CookieBar from '../CookieBar';
 
 describe('<CookieBar />', () => {
   it('should render correctly', () => {
-    const wrapper = mount(<CookieBar />);
+    const wrapper = mount(
+      <CookieBar onAccept={jest.fn()} onDecline={jest.fn()} />,
+    );
     expect(wrapper).toMatchSnapshot();
   });
 });
