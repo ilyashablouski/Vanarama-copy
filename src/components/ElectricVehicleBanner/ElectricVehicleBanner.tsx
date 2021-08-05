a9dca16c0cf5df311eaf4eacd4edce51ce561870
AntonAdamkovich <anton.adamkovich@gmail.com>
2021-08-05 12:37:52 +0300

    Update styles for banners and remove feature flag

--- src/components/ElectricVehicleBanner/ElectricVehicleBanner.tsx
+++ src/components/ElectricVehicleBanner/ElectricVehicleBanner.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import cx from 'classnames';
 import Text from 'core/atoms/text';
-import Cookies from 'js-cookie';
 
 const ElectricVehicleBanner = () => {
-  if (Cookies.get('DIG-6864') !== '1') {
-    return null;
-  }
-
   return (
     <div className={cx('promotion-item', '--secondary')}>
       <Text size="regular" color="white" className="promotion-item-accent-text">
         Free Home Charger With Installation
       </Text>
-      <Text
-        color="white"
-        className="promotion-item-regular-text"
-      >{` Worth £900*`}</Text>
     </div>
   );
 };
