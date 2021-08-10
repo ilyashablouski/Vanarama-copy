afc6333bb0538ce5d75107b025e9ce62cf705dec
Aliaksei Douhal <kcafmil@gmail.com>
2021-08-10 12:36:39 +0300

    DIG-6365 fixed types

--- generated/globalTypes.ts
+++ generated/globalTypes.ts
@@ -559,7 +559,7 @@ export interface LimitedCompanyInputObject {
   addresses?: AddressInputObject[] | null;
   associates?: CompanyAssociateInputObject[] | null;
   bankAccount?: BankAccountInputObject | null;
-  companyNature: string;
+  companyNature?: string | null;
   companyNumber?: string | null;
   companyType?: string | null;
   emailAddress?: EmailAddressInputObject | null;
@@ -785,7 +785,7 @@ export interface SoleTraderCompanyInputObject {
   annualTurnover?: number | null;
   associate?: CompanyAssociateInputObject | null;
   bankAccount?: BankAccountInputObject | null;
-  companyNature: string;
+  companyNature?: string | null;
   companyNumber?: string | null;
   companyType?: string | null;
   emailAddress?: EmailAddressInputObject | null;
