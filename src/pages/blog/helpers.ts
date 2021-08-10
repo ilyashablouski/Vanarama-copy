e9ee03b4a391b78e87835f506cb441a337bef94f
Ilya Shablouski <ilya.shablouski@ozitag.com>
2021-08-10 17:38:08 +0300

    add feature flag DIG-6993

--- src/pages/blog/helpers.ts
+++ src/pages/blog/helpers.ts
@@ -1,3 +1,4 @@
+import Cookies from 'js-cookie';
 import { getBlogBreadCrumbsFromSlug } from '../../utils/breadcrumbs';
 import { IBreadcrumb, IBreadcrumbLink } from '../../types/breadcrumbs';
 import { IMetaDataSection } from '../../types/common';
@@ -5,11 +6,20 @@ import { IMetaDataSection } from '../../types/common';
 function getBreadCrumbsItems(
   metaData: IMetaDataSection,
 ): IBreadcrumbLink[] | null {
+  const isBreadcrumbsFromSlugEnabled = Cookies.get('DIG-6993') === '1';
+
+  if (isBreadcrumbsFromSlugEnabled) {
+    return metaData?.breadcrumbs
+      ? metaData?.breadcrumbs?.map((el: IBreadcrumb) => ({
+          link: { href: el.href || '', label: el.label },
+        }))
+      : getBlogBreadCrumbsFromSlug(metaData.slug);
+  }
   return metaData?.breadcrumbs
     ? metaData?.breadcrumbs?.map((el: IBreadcrumb) => ({
         link: { href: el.href || '', label: el.label },
       }))
-    : getBlogBreadCrumbsFromSlug(metaData.slug);
+    : null;
 }
 
 export default getBreadCrumbsItems;
