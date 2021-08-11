75168da9cd35a713faa6b5219a3052360bcfbcd1
Ilya Shablouski <ilya.shablouski@ozitag.com>
2021-08-11 16:33:04 +0300

    add test for getBreadCrumbsItems function

--- src/utils/__tests__/breadcrumbs.spec.ts
+++ src/utils/__tests__/breadcrumbs.spec.ts
@@ -1,7 +1,47 @@
+import Cookies from 'js-cookie';
 import {
   convertSlugToBreadcrumbsSchema,
   getBlogBreadCrumbsFromSlug,
+  getBreadCrumbsItems,
 } from '../breadcrumbs';
+
+describe('getBreadCrumbsItems', () => {
+  // Delete then feature flag will remove
+  beforeEach(() => {
+    Cookies.set('DIG-6993', '1');
+  });
+
+  it('getBreadCrumbsItems should return correct result', () => {
+    const metaData = {
+      breadcrumbs: null,
+      canonicalUrl:
+        'https://www.vanarama.com/latest-news/mitsubishi-outlander-commercial-review.html',
+      legacyUrl: 'latest-news/mitsubishi-outlander-commercial-review.html',
+      metaDescription:
+        'Vanarama Van Expert Tom Roberts reviews a Mitsubishi Outlander Commercial',
+      metaRobots: null,
+      name: 'Mitsubishi Outlander Commercial',
+      pageType: 'Blog Article',
+      publishedOn: '2021-07-22',
+      schema: null,
+      slug: 'blog/vans/mitsubishi-outlander-commercial-review',
+      title: 'Mitsubishi Outlander Commercial',
+    };
+    const expected = [
+      { link: { href: '/', label: 'Home' } },
+      { link: { href: '/blog', label: 'blog' } },
+      { link: { href: '/blog/vans', label: 'Vans' } },
+      {
+        link: {
+          href: '/blog/vans/mitsubishi-outlander-commercial-review',
+          label: 'Mitsubishi Outlander Commercial Review',
+        },
+      },
+    ];
+
+    expect(getBreadCrumbsItems(metaData)).toStrictEqual(expected);
+  });
+});
 
 describe('getBlogBreadCrumbsFromSlug', () => {
   it('getBreadcrumbSlugs should return correct result', () => {
