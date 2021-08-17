08f9456e994f24b3af0c79be4ec1c65e51975a68
galko <v.galko@agentestudio.com>
2021-08-17 16:21:58 +0300

    add icons for MediaGallery tabs & refactoring

---
+++ src/core/assets/icons/MediaVideo.tsx
@@ -0,0 +1,15 @@
+import { FC, memo } from 'react';
+
+const Svg: FC = memo(() => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+    <g id="Video" transform="translate(-646 -802)">
+      <path
+        id="ios-play-circle"
+        d="M60,48A12,12,0,1,0,72,60,12,12,0,0,0,60,48Zm4.835,12.225-7.915,4.788a.257.257,0,0,1-.387-.225V55.212a.256.256,0,0,1,.387-.225l7.915,4.788A.264.264,0,0,1,64.835,60.225Z"
+        transform="translate(598 754)"
+      />
+    </g>
+  </svg>
+));
+
+export default Svg;
