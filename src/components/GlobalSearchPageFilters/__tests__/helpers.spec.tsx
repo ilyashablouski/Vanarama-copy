48b7ac3bef3030d26e259fc6635c9c1eebb6b496
Aliaksei Douhal <kcafmil@gmail.com>
2021-08-18 15:37:54 +0300

    DIG-6366 added unit tests

--- src/components/GlobalSearchPageFilters/__tests__/helpers.spec.tsx
+++ src/components/GlobalSearchPageFilters/__tests__/helpers.spec.tsx
@@ -4,15 +4,13 @@ import {
   renderPowerEngineSelected,
   renderPowerEngineValue,
   generateQueryObject,
+  buildEnginePowerValues,
 } from '../helpers';
 import { IFiltersData } from '../../../containers/GlobalSearchPageContainer/interfaces';
 
 describe('helpers', () => {
   it('renderBudgetValue should return correct budget value', () => {
     expect(renderBudgetValue('300')).toEqual('£300');
-  });
-  it('renderPowerEngineValue should return correct power value', () => {
-    expect(renderPowerEngineValue('300')).toEqual('300bhp');
   });
   it('renderPowerEngineValue should return correct power value', () => {
     expect(renderPowerEngineValue('300')).toEqual('300bhp');
@@ -31,6 +29,11 @@ describe('helpers', () => {
       budget: '150|550',
       enginePowerBhp: '160|240',
     });
+  });
+  it('buildEnginePowerValues should create array of engine power values', () => {
+    expect(buildEnginePowerValues(280, 280)).toEqual([280, 300]);
+    expect(buildEnginePowerValues(120, 187)).toEqual([120, 140, 160, 180, 200]);
+    expect(buildEnginePowerValues(187, 187)).toEqual([180, 200]);
   });
   describe('renderBudgetSelected', () => {
     it('renderBudgetSelected should return correct selected string with only From value', () => {
