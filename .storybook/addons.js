32d052ba42b835bf07124d94b33daa9c82398c9f
galko <v.galko@agentestudio.com>
2021-08-25 02:05:40 +0300

    migrate from storybook/knobs to storybook/controls

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,7 +1,7 @@
 import { addons } from '@storybook/addons';
 import vanaramaTheme from './vanaramaTheme';
 import '@storybook/addon-actions/register';
-import '@storybook/addon-knobs/register';
+import '@storybook/addon-controls/register';
 import '@storybook/addon-viewport/register';
 
 addons.setConfig({
--- src/core/organisms/lease-scanner/index.story.tsx
+++ src/core/organisms/lease-scanner/index.story.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import base from 'paths.macro';
-import { boolean, withKnobs } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+
 import { atomicDir } from '../../../helpers/atomicDirUtils';
+import { Nullish } from '../../../types/common';
+
 import LeaseScanner from './LeaseScanner';
 
 const leasingProviders = [
@@ -20,62 +22,90 @@ const leasingProviders = [
   'BlackHorse',
 ];
 
+const params = {
+  args: {
+    startLoading: false,
+  },
+  argTypes: {
+    startLoading: {
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
+};
+
+interface ICustomArgs {
+  startLoading?: boolean;
+}
+
 storiesOf(`${atomicDir(base)}/LeaseScanner`, module)
-  .addDecorator(withKnobs)
-  .add('Default', () => (
-    <div className="lease-scanner--sticky-wrap">
-      <LeaseScanner
-        price={229.0}
-        classNameHeading="headingText"
-        orderNowClick={action('Order now')}
-        headingText="PM inc. VAT"
-        leasingProviders={leasingProviders}
-        priceLabel="+£38 Maintenance"
-        endAnimation={() => {}}
-        startLoading={boolean('Start Loading', false)}
-      />
-    </div>
-  ))
-  .add('with checkbox', () => (
-    <div
-      style={{
-        maxWidth: 410,
-        position: 'absolute',
-        bottom: 0,
-        right: 0,
-        width: '100%',
-      }}
-    >
-      <LeaseScanner
-        price={229.0}
-        orderNowClick={action('Order now')}
-        startLoading={boolean('Start Loading', false)}
-        headingText="PM inc. VAT"
-        withCheckBox
-        endAnimation={() => {}}
-        leasingProviders={leasingProviders}
-      />
-    </div>
-  ))
-  .add('with next best price', () => (
-    <div
-      style={{
-        maxWidth: 410,
-        position: 'absolute',
-        bottom: 0,
-        right: 0,
-        width: '100%',
-      }}
-    >
-      <LeaseScanner
-        price={229.0}
-        nextBestPrice="£524.10 PM Ex VAT"
-        orderNowClick={action('Order now')}
-        startLoading={boolean('Start Loading', false)}
-        headingText="PM inc. VAT"
-        withCheckBox
-        endAnimation={() => {}}
-        leasingProviders={leasingProviders}
-      />
-    </div>
-  ));
+  .add(
+    'Default',
+    (args: Nullish<ICustomArgs>) => (
+      <div className="lease-scanner--sticky-wrap">
+        <LeaseScanner
+          price={229.0}
+          startLoading={args?.startLoading ?? false}
+          classNameHeading="headingText"
+          orderNowClick={action('Order now')}
+          headingText="PM inc. VAT"
+          leasingProviders={leasingProviders}
+          priceLabel="+£38 Maintenance"
+          endAnimation={() => {}}
+        />
+      </div>
+    ),
+    params,
+  )
+  .add(
+    'with checkbox',
+    (args: Nullish<ICustomArgs>) => (
+      <div
+        style={{
+          maxWidth: 410,
+          position: 'absolute',
+          bottom: 0,
+          right: 0,
+          width: '100%',
+        }}
+      >
+        <LeaseScanner
+          price={229.0}
+          startLoading={args?.startLoading ?? false}
+          orderNowClick={action('Order now')}
+          headingText="PM inc. VAT"
+          withCheckBox
+          endAnimation={() => {}}
+          leasingProviders={leasingProviders}
+        />
+      </div>
+    ),
+    params,
+  )
+  .add(
+    'with next best price',
+    (args: Nullish<ICustomArgs>) => (
+      <div
+        style={{
+          maxWidth: 410,
+          position: 'absolute',
+          bottom: 0,
+          right: 0,
+          width: '100%',
+        }}
+      >
+        <LeaseScanner
+          price={229.0}
+          startLoading={args?.startLoading ?? false}
+          nextBestPrice="£524.10 PM Ex VAT"
+          orderNowClick={action('Order now')}
+          headingText="PM inc. VAT"
+          withCheckBox
+          endAnimation={() => {}}
+          leasingProviders={leasingProviders}
+        />
+      </div>
+    ),
+    params,
+  );
