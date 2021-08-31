5949d45811d7a65e1c881e96bcf7275f4181857e
Ilya Shablouski <ilya.shablouski@ozitag.com>
2021-08-31 20:14:09 +0300

    edit swiper types & styles

--- src/core/organisms/carousel/interface.ts
+++ src/core/organisms/carousel/interface.ts
@@ -2,6 +2,7 @@ import { IBaseProps } from '../../interfaces/base';
 
 export interface ICarouselProps extends IBaseProps {
   countItems: number;
+  children?: React.ReactNode;
   countShow?: number;
   initialSlideHeight?: number;
 }
