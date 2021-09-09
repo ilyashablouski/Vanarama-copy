2e0b6c7f3b04d70b94b11c5bb100744398d55e12
Ilya Shablouski <ilya.shablouski@ozitag.com>
2021-09-09 20:01:35 +0300

    refactor modalV2 & arrow icons

--- src/core/assets/icons/ChevronForwardOutline.tsx
+++ src/core/assets/icons/ChevronForwardOutline.tsx
@@ -1,9 +1,15 @@
 import React, { FC, memo } from 'react';
 
 const Svg: FC = memo(() => (
-  <svg width="1em" height="1em" viewBox="0 0 512 512">
-    <polyline
-      points="184 112 328 256 184 400"
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 512 512"
+  >
+    <title>Chevron Forward</title>
+    <path
+      d="M184 112l144 144-144 144"
       style={{
         fill: 'none',
         stroke: 'var(--class-color)',
