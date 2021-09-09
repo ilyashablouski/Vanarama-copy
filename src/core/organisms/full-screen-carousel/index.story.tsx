7aa1e522ce01af65f5ee178f9a04e90818b9c3c6
Ilya Shablouski <ilya.shablouski@ozitag.com>
2021-09-09 13:15:02 +0300

    fix button styles in FullScreenIcon & storybook

--- src/core/organisms/full-screen-carousel/index.story.tsx
+++ src/core/organisms/full-screen-carousel/index.story.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import base from 'paths.macro';
 import { storiesOf } from '@storybook/react';
-
-import Button from 'core/atoms/button';
 import FullScreenIcon from 'core/assets/icons/FullScreenIcon';
 import { atomicDir } from '../../../helpers/atomicDirUtils';
 
@@ -14,15 +12,35 @@ storiesOf(`${atomicDir(base)}/FullScreenImageCarousel`, module).add(
     const [isOpenModal, setOpenModal] = useState(true);
 
     return (
-      <>
-        <Button
-          color="teal"
-          icon={<FullScreenIcon />}
-          iconPosition="after"
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '200px',
+          height: '200px',
+          background: 'lightgray',
+        }}
+      >
+        <button
+          type="button"
           onClick={() => {
             setOpenModal(true);
           }}
-        />
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            background: 'rgba(255, 255, 255, .8)',
+            width: '45px',
+            height: '45px',
+            borderRadius: '50%',
+            fontSize: '20px',
+            cursor: 'pointer',
+          }}
+        >
+          <FullScreenIcon />
+        </button>
 
         <div
           style={{
@@ -45,7 +63,7 @@ storiesOf(`${atomicDir(base)}/FullScreenImageCarousel`, module).add(
             setOpenModal={() => setOpenModal(false)}
           />
         </div>
-      </>
+      </div>
     );
   },
 );
