db645152ea42dbe9445f488ed7ae37d43f1ff118
Ilya Shablouski <ilya.shablouski@ozitag.com>
2021-10-19 15:27:58 +0300

    fix: activePartnership cookie warning message
    Added sameSite: 'lax' attribute & add options for remove cookies functions

--- src/utils/partnerProperties.ts
+++ src/utils/partnerProperties.ts
@@ -73,17 +73,19 @@ export function setPartnerProperties(
   if (data) {
     Cookies.set(PARTNER_COOKIE_NAME, data, {
       expires,
+      sameSite: 'lax',
       secure: true,
     });
   }
 }
 
 export function removePartnerProperties() {
-  Cookies.remove(PARTNER_COOKIE_NAME);
+  Cookies.remove(PARTNER_COOKIE_NAME, { sameSite: 'lax', secure: true });
 }
 
 export function setSessionFuelTypes(fuelTypes: string[]) {
   Cookies.set(CUSTOM_SESSION_FUEL_TYPES, fuelTypes, {
+    sameSite: 'lax',
     secure: true,
   });
 }
@@ -102,7 +104,10 @@ export function clearInactiveSessionFuelTypes() {
     Cookies.get(CUSTOM_SESSION_FUEL_TYPES) &&
     !getSessionStorage(PARTNER_SESSION_ACTIVE)
   ) {
-    Cookies.remove(CUSTOM_SESSION_FUEL_TYPES);
+    Cookies.remove(CUSTOM_SESSION_FUEL_TYPES, {
+      sameSite: 'lax',
+      secure: true,
+    });
   }
 }
 
