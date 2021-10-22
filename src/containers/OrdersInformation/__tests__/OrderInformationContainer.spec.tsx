cd758eb8db06cb79e8d73c6419efcc03d8ace3ba
ABmarketing-cloud <71643488+ABmarketing-cloud@users.noreply.github.com>
2021-10-22 14:09:09 +0300

    DIG-7593 add mock data for OrderInformationContainer test

--- src/containers/OrdersInformation/__tests__/OrderInformationContainer.spec.tsx
+++ src/containers/OrdersInformation/__tests__/OrderInformationContainer.spec.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import preloadAll from 'jest-next-dynamic';
 import renderer from 'react-test-renderer';
 import OrderInformationContainer from '../OrderInformationContainer';
+import { LeaseTypeEnum } from '../../../../generated/globalTypes';
+import { GetMyOrders_myOrders } from '../../../../generated/GetMyOrders';
 
 jest.mock('../../../hooks/useImperativeQuery');
 
@@ -14,6 +16,36 @@ jest.mock('next/router', () => ({
     };
   },
 }));
+
+const mockOrders = [
+  {
+    uuid: '4fe8a6de-32f3-42eb-a625-cd757f83bc39',
+    leaseType: LeaseTypeEnum.BUSINESS,
+    partyUuid: '4dae0697-04e6-499f-a69f-1f93c5f35484',
+    personUuid: 'c9f58592-4141-4e14-9dfe-1179651a2c7d',
+    salesChannel: '0',
+    status: 'credit',
+  },
+] as GetMyOrders_myOrders[];
+
+const mockQuoters = [
+  {
+    uuid: '4fe8a6de-32f3-42eb-a625-cd757f83bc39',
+    leaseType: LeaseTypeEnum.PERSONAL,
+    partyUuid: '4dae0697-04e6-499f-a69f-1f93c5f35484',
+    personUuid: 'c9f58592-4141-4e14-9dfe-1179651a2c7d',
+    salesChannel: '0',
+    status: 'credit',
+  },
+  {
+    uuid: '4fe8a6de-32f3-42eb-a625-cd757f83bc39',
+    leaseType: LeaseTypeEnum.PERSONAL,
+    partyUuid: '4dae0697-04e6-499f-a69f-1f93c5f35484',
+    personUuid: 'c9f58592-4141-4e14-9dfe-1179651a2c7d',
+    salesChannel: '0',
+    status: 'credit',
+  },
+] as GetMyOrders_myOrders[];
 
 describe('<OrderInformationContainer />', () => {
   beforeEach(async () => {
@@ -32,8 +64,8 @@ describe('<OrderInformationContainer />', () => {
       return renderer
         .create(
           <OrderInformationContainer
-            orders={[]}
-            quotes={[]}
+            orders={mockOrders}
+            quotes={mockQuoters}
             uuid="aa08cca2-5f8d-4b8c-9506-193d9c32e05f"
           />,
         )
