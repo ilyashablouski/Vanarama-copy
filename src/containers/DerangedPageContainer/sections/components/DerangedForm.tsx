689d2113926ee70476437e1e28f8d8e1ffb59f56
NewAlexey <alexey.krupenia@gmail.com>
2021-11-03 19:31:16 +0300

    create constants with placeholders and labels values

--- src/containers/DerangedPageContainer/sections/components/DerangedForm.tsx
+++ src/containers/DerangedPageContainer/sections/components/DerangedForm.tsx
@@ -11,6 +11,10 @@ import {
 import ModalFormSuccessMessage from './ModalFormSuccessMessage';
 import { IGoldrushFromValues } from '../../../../components/GoldrushForm/interfaces';
 import ErrorMessages from '../../../../models/enum/ErrorMessages';
+import {
+  DERANGED_FORM_LABELS_VALUES,
+  DERANGED_FORM_PLACEHOLDERS_VALUES,
+} from '../constants';
 
 const Image = dynamic(() => import('core/atoms/image'), {
   loading: () => <Skeleton count={2} />,
@@ -83,16 +87,8 @@ const DerangedForm: React.FC<IDerangedForm> = ({
             onSubmit={values => onSubmit(values)}
             isSubmitting={loading}
             callBack
-            isLabelsShown={{
-              fullName: false,
-              email: false,
-              phoneNumber: false,
-            }}
-            isPlaceholdersShown={{
-              fullName: true,
-              email: true,
-              phoneNumber: true,
-            }}
+            isLabelsShown={DERANGED_FORM_LABELS_VALUES}
+            isPlaceholdersShown={DERANGED_FORM_PLACEHOLDERS_VALUES}
           />
           <Text className="drawer__text-footer" size="xsmall" color="dark">
             Price is subject to change based on your lease term
