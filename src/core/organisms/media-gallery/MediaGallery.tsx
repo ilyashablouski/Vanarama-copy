55989ef84775bb0b7da0df1d3855525d3ed6ab0f
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2021-11-08 17:55:45 +0000

    Enable lazy load for ImacaViewer component

--- src/core/organisms/media-gallery/MediaGallery.tsx
+++ src/core/organisms/media-gallery/MediaGallery.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useMemo } from 'react';
+import { LazyLoadComponent } from 'react-lazy-load-image-component';
 import cx from 'classnames';
 
 import Icon from 'core/atoms/icon';
@@ -15,6 +16,7 @@ import MediaPicture from 'core/assets/icons/MediaPicture';
 
 import ColorWheelIcon from 'core/assets/icons/ColorWheel';
 import Text from 'core/atoms/text';
+import { isServerRenderOrAppleDevice } from '../../../utils/deviceType';
 import { IMediaGalleryProps } from './interfaces';
 
 import ImacaViewer from './ImacaViewer';
@@ -68,13 +70,18 @@ function MediaGallery({
           <TabPanels className="media-gallery__content">
             {shouldRenderImaca && (
               <TabPanel index={0}>
-                <ImacaViewer
-                  isOpenColourSelect={isOpenColourSelect}
-                  colour={colour}
-                  setColour={setColour}
-                  assets={imacaAssets!}
-                  upscaleCanvas={isCar}
-                />
+                <LazyLoadComponent
+                  placeholder={<div className="imaca-viewer-placeholder" />}
+                  visibleByDefault={isServerRenderOrAppleDevice}
+                >
+                  <ImacaViewer
+                    isOpenColourSelect={isOpenColourSelect}
+                    colour={colour}
+                    setColour={setColour}
+                    assets={imacaAssets!}
+                    upscaleCanvas={isCar}
+                  />
+                </LazyLoadComponent>
               </TabPanel>
             )}
             <TabPanel index={1}>
