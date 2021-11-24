eb900a79a4569d892d8055f57b9ac9b9c3af61f5
iamgnlc <gianluca.agnocchetti@gmail.com>
2021-11-24 14:34:58 +0000

    revert old component

--- src/core/atoms/image/Image.tsx
+++ src/core/atoms/image/Image.tsx
@@ -1,10 +1,14 @@
 import React, { FC } from 'react';
 import cx from 'classnames';
-import Image from 'next/image';
 
 import { IImageProps } from './interfaces';
 
-const ImageV2: FC<IImageProps> = props => {
+import {
+  DEFAULT_OPTIMISATION,
+  optimiseImage,
+} from '../../../helpers/imageOptimiseUtils';
+
+const Image: FC<IImageProps> = props => {
   const {
     className,
     size = 'expand',
@@ -14,21 +18,54 @@ const ImageV2: FC<IImageProps> = props => {
     round,
     plain,
     inline,
+    optimisedHost,
+    optimisationOptions,
+    lazyLoad,
     dataTestId,
   } = props;
 
-  let { src } = props;
+  const { src } = props;
 
-  if (src.search(/^http[s]?:/) === -1) {
-    src = `https:${src}`;
+  let srcset;
+  let srcDefault = src;
+  let src1200;
+  let src800;
+  let src320;
+
+  // Check if image should be optimised.
+  if (optimisedHost) {
+    srcDefault = optimiseImage(optimisedHost, src, {
+      ...DEFAULT_OPTIMISATION,
+      ...optimisationOptions,
+    });
+    src1200 = optimiseImage(optimisedHost, src, {
+      ...DEFAULT_OPTIMISATION,
+      width: 1200,
+      height: 1200,
+      ...optimisationOptions,
+    });
+    src800 = optimiseImage(optimisedHost, src, {
+      ...DEFAULT_OPTIMISATION,
+      width: 800,
+      height: 800,
+      quality: 70,
+      ...optimisationOptions,
+    });
+    src320 = optimiseImage(optimisedHost, src, {
+      ...DEFAULT_OPTIMISATION,
+      width: 320,
+      height: 320,
+      quality: 60,
+      ...optimisationOptions,
+    });
+
+    srcset = `${src320} 320w, ${src800} 800w, ${src1200} 1200w`;
   }
 
   const onError = (e: any) => {
     e.target.srcset = '';
     e.target.src = `${process.env.HOST_DOMAIN}/vehiclePlaceholder.jpg`;
   };
-
-  const layout = !width || !height ? 'fill' : 'responsive';
 
   return (
     <div
@@ -39,20 +76,24 @@ const ImageV2: FC<IImageProps> = props => {
         '-plain': plain,
         '-inline': inline,
       })}
-      data-testid={dataTestId}
     >
-      <Image
+      <img
+        loading={lazyLoad ? 'lazy' : 'eager'}
+        srcSet={srcset}
+        sizes="(min-width:320px) 800px, 1200px"
         alt={alt}
         width={width}
         height={height}
-        src={src}
+        className="image--native"
+        src={srcDefault || src}
         onError={onError}
-        layout={layout}
+        data-cfasync="false"
+        data-testid={dataTestId}
       />
     </div>
   );
 };
 
-ImageV2.displayName = 'ImageV2';
+Image.displayName = 'Image';
 
-export default React.memo(ImageV2);
+export default React.memo(Image);
