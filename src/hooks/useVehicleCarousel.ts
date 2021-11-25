e104ad8aa963259f2ff30a4799bfccd49ed2f2c8
Aliaksei Douhal <kcafmil@gmail.com>
2021-11-25 18:21:26 +0300

    DIG-8042 DIG-8043 implemented card data queries

--- src/hooks/useVehicleCarousel.ts
+++ src/hooks/useVehicleCarousel.ts
@@ -1,46 +1,30 @@
 import { useEffect, useState } from 'react';
 import { useApolloClient } from '@apollo/client';
 import Cookies from 'js-cookie';
-import { Nullish } from '../types/common';
-import { ProductCardData } from '../../generated/ProductCardData';
-import { GetDerivatives } from '../../generated/GetDerivatives';
-import { VehicleListUrl_vehicleList as IVehicleList } from '../../generated/VehicleListUrl';
-import { specialOffersForBlogPageRequest } from '../utils/offers';
-import { VehicleTypeEnum } from '../../generated/globalTypes';
+import { vehicleCarouselForBlogPageRequest } from '../utils/offers';
+import { ProductDerivativeFilter } from '../../generated/globalTypes';
 import { isBlogCarPagesCarouselFeatureFlagEnabled } from '../utils/helpers';
+import { IBlogCarouselCard } from '../components/BlogCarousel/interface';
 
-export default function useVehicleCarousel(vehicleType: VehicleTypeEnum) {
+export default function useVehicleCarousel(
+  productFilter: ProductDerivativeFilter,
+) {
   const client = useApolloClient();
 
-  const [productsCarData, setProductsCarData] = useState<
-    Nullish<ProductCardData>
-  >(null);
-  const [productsCarDerivativesData, setProductsCarDerivativesData] = useState<
-    Nullish<GetDerivatives>
-  >(null);
-  const [vehicleListUrlState, setVehicleListUrlState] = useState<
-    Nullish<IVehicleList>
-  >(null);
+  const [vehiclesList, setVehiclesList] = useState<IBlogCarouselCard[]>([]);
 
   useEffect(() => {
     if (isBlogCarPagesCarouselFeatureFlagEnabled(Cookies)) {
       const getDataForCarousel = async () => {
-        const {
-          productsCar,
-          productsCarDerivatives,
-          vehicleListUrlData,
-        } = await specialOffersForBlogPageRequest(client, vehicleType);
-        setProductsCarData(productsCar);
-        setProductsCarDerivativesData(productsCarDerivatives);
-        setVehicleListUrlState(vehicleListUrlData);
+        const vehicleCarouselList = await vehicleCarouselForBlogPageRequest(
+          client,
+          productFilter,
+        );
+        setVehiclesList(vehicleCarouselList || []);
       };
       getDataForCarousel();
     }
-  }, [client, vehicleType]);
+  }, [client]);
 
-  return {
-    productsCarData,
-    productsCarDerivativesData,
-    vehicleListUrlState,
-  };
+  return vehiclesList;
 }
