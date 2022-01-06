ef681bd696140a2fdefd00bf487e43cb4ce6addc
galko <v.galko@agentestudio.com>
2022-01-06 13:23:51 +0300

    use feature flag to display Jan sale benefit

--- src/core/organisms/benefits-bar/constants.tsx
+++ src/core/organisms/benefits-bar/constants.tsx
@@ -1,8 +1,10 @@
 import PriceLowest from 'core/assets/icons/PriceLowest';
 import ReturnCircle from 'core/assets/icons/ReturnCircle';
-// import BreakdownCar from 'core/assets/icons/BreakdownCar';
+import BreakdownCar from 'core/assets/icons/BreakdownCar';
 import PoundCircle from 'core/assets/icons/PoundCircle';
 import DeliveryCar from 'core/assets/icons/DeliveryCar';
+
+import { isJanSaleCampaignEnabled } from '../../../utils/helpers';
 
 // eslint-disable-next-line import/prefer-default-export
 export const BENEFIT_LIST = [
@@ -23,14 +25,15 @@ export const BENEFIT_LIST = [
       />
     ),
   },
-  // {
-  //   title: 'Road Tax & Roadside Assistance Included',
-  //   icon: <BreakdownCar />,
-  // },
-  {
-    title: '£250 Cashback On Every Vehicle',
-    icon: <PoundCircle />,
-  },
+  isJanSaleCampaignEnabled()
+    ? {
+        title: '£250 Cashback On Every Vehicle',
+        icon: <PoundCircle />,
+      }
+    : {
+        title: 'Road Tax & Roadside Assistance Included',
+        icon: <BreakdownCar />,
+      },
   {
     title: 'FREE & Fast Delivery',
     icon: <DeliveryCar />,
