ce65d016f6accadf2d8a34ba77d7961127e48006
iamgnlc <gianluca.agnocchetti@gmail.com>
2022-01-11 15:35:21 +0000

    add CF img optimisation on benefit bar image

--- src/core/organisms/benefits-bar/constants.tsx
+++ src/core/organisms/benefits-bar/constants.tsx
@@ -2,6 +2,9 @@ import PriceLowest from 'core/assets/icons/PriceLowest';
 import ReturnCircle from 'core/assets/icons/ReturnCircle';
 import BreakdownCar from 'core/assets/icons/BreakdownCar';
 import DeliveryCar from 'core/assets/icons/DeliveryCar';
+import Image from 'core/atoms/image/Image';
+
+// import tpLogo from '../../../../public/Assets/images/benefits-bar/trust-pilot-logo.png';
 
 // eslint-disable-next-line import/prefer-default-export
 export const BENEFIT_LIST = [
@@ -16,10 +19,13 @@ export const BENEFIT_LIST = [
   {
     title: 'Rated Excellent',
     icon: (
-      <img
-        alt="Trust pilot logo"
-        src="/Assets/images/benefits-bar/trust-pilot-logo.png"
+      <Image
+        optimisedHost={process.env.IMG_OPTIMISATION_HOST}
+        src={`${process.env.HOST_DOMAIN}/Assets/images/benefits-bar/trust-pilot-logo.png`}
+        size="expand"
+        plain
       />
+      // <ImageV2 width={61} height={25} alt="Trust pilot logo" src={tpLogo} />
     ),
   },
   {
