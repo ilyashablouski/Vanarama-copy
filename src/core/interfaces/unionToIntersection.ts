1d0fb72b6b19f9abf4262562d50cc7ad72108e1e
zhuravlevanatalia <zhuravleva.977@yandex.by>
2022-01-31 12:22:44 +0300

    DIG-8632 add comments

--- src/core/interfaces/unionToIntersection.ts
+++ src/core/interfaces/unionToIntersection.ts
@@ -1,3 +1,6 @@
+/**
+ * Use it collect filter types for each entity described so as not to do it manually
+ */
 export type UnionToIntersection<U> = (U extends any
 ? (k: U) => void
 : never) extends (k: infer I) => void
