d2983a282d25987b28046539dd0de9eb29f13185
Alina Hrybouskaja <alina.kala4ik@gmail.com>
2022-03-11 19:33:10 +0400

    DIG-9069 add Lead Text and Featured Sections x2

--- src/containers/CarHubPageContainer/CarHubPageContainer.tsx
+++ src/containers/CarHubPageContainer/CarHubPageContainer.tsx
@@ -1,24 +1,132 @@
-import { memo, FC } from 'react';
+import React, { memo, FC } from 'react';
 import SchemaJSON from 'core/atoms/schema-json';
 import Heading from 'core/atoms/heading';
+import Media from 'core/atoms/media';
+import ImageV2 from 'core/atoms/image/ImageV2';
+import ReactMarkdown from 'react-markdown/with-html';
+import dynamic from 'next/dynamic';
 import Head from '../../components/Head/Head';
 import { IPageWithData } from '../../types/common';
-import { GenericPageQuery } from '../../../generated/GenericPageQuery';
+import {
+  GenericPageQuery,
+  GenericPageQuery_genericPage_sectionsAsArray_featured,
+  GenericPageQuery_genericPage_sectionsAsArray_leadText,
+} from '../../../generated/GenericPageQuery';
 import getTitleTag from '../../utils/getTitleTag';
 import CardsSectionCarousel from '../../components/CardsSectionCarousel';
 import { normalizeString } from '../../utils/data';
+import HeadingSection from '../../components/HeadingSection';
+import { getFeaturedClassPartial } from '../../utils/layout';
+import RouterLink from '../../components/RouterLink';
+import Skeleton from '../../components/Skeleton';
+
+const Text = dynamic(() => import('core/atoms/text'), {
+  loading: () => <Skeleton count={1} />,
+});
 
 type IProps = IPageWithData<{
   data: GenericPageQuery;
   dataUiTestId?: string;
 }>;
 
+const getFeaturesSection = (
+  features:
+    | (GenericPageQuery_genericPage_sectionsAsArray_featured | null)[]
+    | null,
+) => {
+  return features?.map(featured => (
+    <section className={`row:${getFeaturedClassPartial(featured)}`}>
+      {featured?.video ? (
+        <Media
+          src={featured?.video || ''}
+          width="100%"
+          height="360px"
+          dataUiTestId={`car-leasing-page_${normalizeString(
+            featured?.title,
+          )}_media`}
+        />
+      ) : (
+        <ImageV2
+          quality={60}
+          objectFit="cover"
+          width={featured?.image?.file?.details.image.width ?? 1000}
+          height={featured?.image?.file?.details.image.height ?? 650}
+          src={
+            featured?.image?.file?.url ||
+            'https://source.unsplash.com/collection/2102317/1000x650?sig=40349'
+          }
+          dataUiTestId={`car-leasing-page_${normalizeString(featured?.title)}`}
+        />
+      )}
+      <div className="-inset -middle -col-400">
+        <Heading
+          dataUiTestId={`car-leasing-page_${normalizeString(featured?.title)}`}
+          size="regular"
+          color="black"
+          tag={
+            getTitleTag(
+              featured?.titleTag || 'p',
+            ) as keyof JSX.IntrinsicElements
+          }
+        >
+          {featured?.title}
+        </Heading>
+        <div className="markdown">
+          <ReactMarkdown
+            allowDangerousHtml
+            source={featured?.body || ''}
+            renderers={{
+              link: props => {
+                const { href, children } = props;
+                return <RouterLink link={{ href, label: children }} />;
+              },
+              heading: props => (
+                <Text {...props} size="lead" color="darker" tag="h3" />
+              ),
+              paragraph: props => <Text {...props} tag="p" color="darker" />,
+            }}
+          />
+        </div>
+      </div>
+    </section>
+  ));
+};
+
+const getHeadingSection = (
+  headingSection: GenericPageQuery_genericPage_sectionsAsArray_leadText | null,
+  isLargeText: boolean,
+) => (
+  <HeadingSection
+    titleTag={headingSection?.titleTag}
+    header={headingSection?.heading}
+    description={headingSection?.description}
+    dataUiTestId="car-leasing-page_features1-heading-section"
+    largeText={isLargeText}
+  />
+);
+
 const CarHubPageContainer: FC<IProps> = ({ data, dataUiTestId }) => {
   const { sectionsAsArray } = data?.genericPage;
   const cards = sectionsAsArray?.cards?.[0];
 
+  const features1LeadTextSection = sectionsAsArray?.leadText?.[1];
+  const features2LeadTextSection = sectionsAsArray?.leadText?.[2];
+
+  const features1 = sectionsAsArray?.featured?.slice(0, 5);
+  const features2 = sectionsAsArray?.featured?.slice(5);
+
   return (
     <>
+      {features1LeadTextSection &&
+        getHeadingSection(features1LeadTextSection, true)}
+
+      {features1 && getFeaturesSection(features1)}
+
+      {features2LeadTextSection &&
+        getHeadingSection(features2LeadTextSection, true)}
+
+      {features2 && getFeaturesSection(features2)}
+
       {cards?.cards?.length && (
         <div className="row:bg-lighter">
           <Heading
