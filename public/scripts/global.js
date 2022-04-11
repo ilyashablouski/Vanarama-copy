4fc52f7e47db1d76e775f3e3ad418fdabdb6eb7b
ilyashablouski <ilya.shablovskiy@gmail.com>
2022-04-11 14:53:08 +0300

    DIG-9489 convert to es5 console error message to fix rollbar issue with quotes

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -3,14 +3,12 @@ function addAdditionalData(data) {
     JSON.parse(data);
   } catch (error) {
     // eslint-disable-next-line no-console
-    console.error(`additionalData doesn't contain JSON\n${error}`);
+    console.error("additionalData doesn't contain JSON\n".concat(error));
     return;
   }
-  // eslint-disable-next-line no-undef
   window.localStorage.setItem('additionalData', data);
 }
 
-// eslint-disable-next-line no-undef
 window.vanarama = {
   addAdditionalData,
 };
