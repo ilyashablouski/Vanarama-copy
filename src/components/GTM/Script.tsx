fd3bcd9a62801a87fdcbe5756ca0eb2dbcea18a2
Yuliya Novik <novik.yuliya.1@gmail.com>
2022-04-19 10:06:21 +0300

    prevent duplicated injection of tracking scripts

--- src/components/GTM/Script.tsx
+++ src/components/GTM/Script.tsx
@@ -3,10 +3,16 @@ import React, { FC, memo } from 'react';
 const Script: FC = () => {
   const code = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${process.env.GTM_ID}');`;
 
+  const gtmUrl = 'www.googletagmanager.com/gtm.js';
+
   const codeWithCustomAttribute = code.replace(
     'j.async=true;',
     "j.async=true;j.setAttribute('data-cfasync',false);",
   );
+
+  if (document.querySelector(`script[src*="${gtmUrl}"]`)) {
+    return null;
+  }
 
   return process.env.GTM_ID ? (
     <script
